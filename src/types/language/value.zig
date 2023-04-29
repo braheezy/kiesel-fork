e7cb5b0733794ebba82d3a187908b63ab014b546
Linus Groh <mail@linusgroh.de>
2023-04-30 00:37:40 +0100

    types: Stub out Value.isArray()

--- src/types/language/value.zig
+++ src/types/language/value.zig
@@ -8,6 +8,8 @@ const Symbol = @import("Symbol.zig");
 /// 6.1 ECMAScript Language Types
 /// https://tc39.es/ecma262/#sec-ecmascript-language-types
 pub const Value = union(enum) {
+    const Self = @This();
+
     /// 6.1.1 The Undefined Type
     /// https://tc39.es/ecma262/#sec-ecmascript-language-types-undefined-type
     undefined,
@@ -89,6 +91,28 @@ pub const Value = union(enum) {
 
     pub fn fromObject(object: Object) Value {
         return .{ .object = object };
+    }
+
+    /// 7.2.2 IsArray ( argument )
+    /// https://tc39.es/ecma262/#sec-isarray
+    pub fn isArray(self: Self) !bool {
+        // 1. If argument is not an Object, return false.
+        if (!self == .object)
+            return false;
+
+        // TODO: 2. If argument is an Array exotic object, return true.
+        if (false)
+            return true;
+
+        // TODO: 3. If argument is a Proxy exotic object, then
+        if (false) {
+            // TODO: a. Perform ? ValidateNonRevokedProxy(argument).
+            // TODO: b. Let proxyTarget be argument.[[ProxyTarget]].
+            // TODO: c. Return ? IsArray(proxyTarget).
+        }
+
+        // 4. Return false.
+        return false;
     }
 };
 
