d41b73fb8a090b9e34d0fd9f3e25fbcbcd77397e
Linus Groh <mail@linusgroh.de>
2023-04-29 22:27:56 +0100

    kiesel: Export more language types in the module

--- src/kiesel.zig
+++ src/kiesel.zig
@@ -3,12 +3,16 @@ const execution = @import("execution.zig");
 const language = @import("language.zig");
 const types = @import("types.zig");
 
+pub const BigInt = types.BigInt;
 pub const Agent = execution.Agent;
 pub const Completion = types.Completion;
+pub const Number = types.Number;
+pub const Object = types.Object;
 pub const PropertyDescriptor = types.PropertyDescriptor;
 pub const PropertyKey = types.PropertyKey;
 pub const Realm = execution.Realm;
 pub const Script = language.Script;
+pub const Symbol = types.Symbol;
 pub const Value = types.Value;
 
 test {
