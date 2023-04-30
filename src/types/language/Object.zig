733fd6fbab057ba68906469d4d11a731fd3ff0dd
Linus Groh <mail@linusgroh.de>
2023-04-30 18:34:51 +0100

    types: Consistently name Object self parameter 'self'

--- src/types/language/Object.zig
+++ src/types/language/Object.zig
@@ -17,40 +17,40 @@ pub const InternalMethods = @import("Object/InternalMethods.zig");
 pub const PropertyKey = @import("Object/PropertyKey.zig").PropertyKey;
 pub const PropertyStorage = @import("Object/PropertyStorage.zig");
 
-const Object = @This();
+const Self = @This();
 
 ptr: *anyopaque,
-data: *Object.Data,
+data: *Data,
 
-pub fn as(object: Object, comptime T: type) *T {
-    return @ptrCast(*T, @alignCast(@alignOf(T), object.ptr));
+pub fn as(self: Self, comptime T: type) *T {
+    return @ptrCast(*T, @alignCast(@alignOf(T), self.ptr));
 }
 
 // Helper functions so we don't have to say 'data' all the time
 
-pub fn prototype(self: Object) *?Object {
+pub fn prototype(self: Self) *?Self {
     return &self.data.prototype;
 }
 
-pub fn extensible(self: Object) *bool {
+pub fn extensible(self: Self) *bool {
     return &self.data.extensible;
 }
 
-pub fn agent(self: Object) *Agent {
+pub fn agent(self: Self) *Agent {
     return self.data.agent;
 }
 
-pub fn internalMethods(self: Object) *InternalMethods {
+pub fn internalMethods(self: Self) *InternalMethods {
     return &self.data.internal_methods;
 }
 
-pub fn propertyStorage(self: Object) *PropertyStorage {
+pub fn propertyStorage(self: Self) *PropertyStorage {
     return &self.data.property_storage;
 }
 
 /// 7.1.1.1 OrdinaryToPrimitive ( O, hint )
 /// https://tc39.es/ecma262/#sec-ordinarytoprimitive
-pub fn ordinaryToPrimitive(object: Object, hint: PreferredType) !Value {
+pub fn ordinaryToPrimitive(self: Self, hint: PreferredType) !Value {
     const method_names = switch (hint) {
         // 1. If hint is string, then
         //     a. Let methodNames be « "toString", "valueOf" ».
@@ -63,12 +63,12 @@ pub fn ordinaryToPrimitive(object: Object, hint: PreferredType) !Value {
     // 3. For each element name of methodNames, do
     for (method_names) |name| {
         // a. Let method be ? Get(O, name).
-        const method = try object.get(PropertyKey.fromString(name));
+        const method = try self.get(PropertyKey.fromString(name));
 
         // b. If IsCallable(method) is true, then
         if (method.isCallable()) {
             // i. Let result be ? Call(method, O).
-            const result = try method.callAssumeCallableNoArgs(Value.fromObject(object));
+            const result = try method.callAssumeCallableNoArgs(Value.fromObject(self));
 
             // ii. If result is not an Object, return result.
             if (result != .object)
@@ -82,23 +82,23 @@ pub fn ordinaryToPrimitive(object: Object, hint: PreferredType) !Value {
 
 /// 7.2.5 IsExtensible ( O )
 /// https://tc39.es/ecma262/#sec-isextensible-o
-pub fn isExtensible(object: Object) !bool {
+pub fn isExtensible(self: Self) !bool {
     // 1. Return ? O.[[IsExtensible]]().
-    return object.internalMethods().isExtensible(object);
+    return self.internalMethods().isExtensible(self);
 }
 
 /// 7.3.2 Get ( O, P )
 /// https://tc39.es/ecma262/#sec-get-o-p
-pub fn get(object: Object, property_key: PropertyKey) !Value {
+pub fn get(self: Self, property_key: PropertyKey) !Value {
     // 1. Return ? O.[[Get]](P, O).
-    return object.internalMethods().get(object, property_key, Value.fromObject(object));
+    return self.internalMethods().get(self, property_key, Value.fromObject(self));
 }
 
 /// 7.3.4 Set ( O, P, V, Throw )
 /// https://tc39.es/ecma262/#sec-set-o-p-v-throw
-pub fn set(object: Object, property_key: PropertyKey, value: Value, throw: enum { throw, ignore }) !void {
+pub fn set(self: Self, property_key: PropertyKey, value: Value, throw: enum { throw, ignore }) !void {
     // 1. Let success be ? O.[[Set]](P, V, O).
-    const success = try object.internalMethods().set(object, property_key, value, Value.fromObject(object));
+    const success = try self.internalMethods().set(self, property_key, value, Value.fromObject(self));
 
     // 2. If success is false and Throw is true, throw a TypeError exception.
     if (!success and throw == .throw)
@@ -109,10 +109,10 @@ pub fn set(object: Object, property_key: PropertyKey, value: Value, throw: enum 
 
 /// 7.3.5 CreateDataProperty ( O, P, V )
 /// https://tc39.es/ecma262/#sec-createdataproperty
-pub fn createDataProperty(object: Object, property_key: PropertyKey, value: Value) !bool {
+pub fn createDataProperty(self: Self, property_key: PropertyKey, value: Value) !bool {
     // 1. Let newDesc be the PropertyDescriptor { [[Value]]: V, [[Writable]]: true, [[Enumerable]]: true, [[Configurable]]: true }.
     const new_descriptor = PropertyDescriptor{ .value = value, .writable = true, .enumerable = true, .configurable = true };
 
     // 2. Return ? O.[[DefineOwnProperty]](P, newDesc).
-    return object.internalMethods().defineOwnProperty(object, property_key, new_descriptor);
+    return self.internalMethods().defineOwnProperty(self, property_key, new_descriptor);
 }
