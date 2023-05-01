40635cc2e477425497549917ff5b4105a7b4147f
Linus Groh <mail@linusgroh.de>
2023-05-01 14:53:10 +0100

    types: Implement Symbol.format()

--- src/types/language/Symbol.zig
+++ src/types/language/Symbol.zig
@@ -1,7 +1,9 @@
 //! 6.1.5 The Symbol Type
 //! https://tc39.es/ecma262/#sec-ecmascript-language-types-symbol-type
 
-const Symbol = @This();
+const std = @import("std");
+
+const Self = @This();
 
 pub const Id = usize;
 
@@ -10,3 +12,35 @@ id: Id,
 
 /// [[Description]]
 description: ?[]const u8,
+
+pub fn format(
+    self: Self,
+    comptime fmt: []const u8,
+    options: std.fmt.FormatOptions,
+    writer: anytype,
+) !void {
+    _ = fmt;
+    _ = options;
+    try writer.writeAll("Symbol(");
+    if (self.description) |description| {
+        try writer.writeAll("\"");
+        try writer.writeAll(description);
+        try writer.writeAll("\"");
+    }
+    try writer.writeAll(")");
+}
+
+test "format" {
+    const test_cases = [_]struct { Self, []const u8 }{
+        .{ .{ .id = 0, .description = null }, "Symbol()" },
+        .{ .{ .id = 1, .description = "" }, "Symbol(\"\")" },
+        .{ .{ .id = 2, .description = "foo" }, "Symbol(\"foo\")" },
+    };
+    for (test_cases) |test_case| {
+        const symbol = test_case[0];
+        const expected = test_case[1];
+        const string = try std.fmt.allocPrint(std.testing.allocator, "{}", .{symbol});
+        defer std.testing.allocator.free(string);
+        try std.testing.expectEqualStrings(expected, string);
+    }
+}
