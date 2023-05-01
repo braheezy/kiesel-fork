a392f402f5cb96c81483797a23ce351fe0d3c380
Linus Groh <mail@linusgroh.de>
2023-05-01 14:51:47 +0100

    types: Implement Number.format()

--- src/types/language/number.zig
+++ src/types/language/number.zig
@@ -13,6 +13,30 @@ pub const Number = union(enum) {
     // i32 internally.
     i32: i32,
 
+    pub fn format(
+        self: Self,
+        comptime fmt: []const u8,
+        options: std.fmt.FormatOptions,
+        writer: anytype,
+    ) !void {
+        _ = fmt;
+        _ = options;
+        switch (self) {
+            .f64 => |x| {
+                if (std.math.isNan(x)) {
+                    try writer.writeAll("NaN");
+                } else if (std.math.isPositiveInf(x)) {
+                    try writer.writeAll("Infinity");
+                } else if (std.math.isNegativeInf(x)) {
+                    try writer.writeAll("-Infinity");
+                } else {
+                    try writer.print("{d}", .{x});
+                }
+            },
+            .i32 => |x| try writer.print("{d}", .{x}),
+        }
+    }
+
     pub fn from(number: anytype) Self {
         switch (@typeInfo(@TypeOf(number))) {
             .Int, .ComptimeInt => return .{ .i32 = @as(i32, number) },
@@ -42,3 +66,23 @@ pub const Number = union(enum) {
         };
     }
 };
+
+test "format" {
+    const test_cases = [_]struct { Number, []const u8 }{
+        .{ Number.from(0), "0" },
+        .{ Number.from(-0.0), "-0" },
+        .{ Number.from(123), "123" },
+        .{ Number.from(123.456), "123.456" },
+        .{ Number.from(-42), "-42" },
+        .{ Number.from(std.math.nan(f64)), "NaN" },
+        .{ Number.from(std.math.inf(f64)), "Infinity" },
+        .{ Number.from(-std.math.inf(f64)), "-Infinity" },
+    };
+    for (test_cases) |test_case| {
+        const number = test_case[0];
+        const expected = test_case[1];
+        const string = try std.fmt.allocPrint(std.testing.allocator, "{}", .{number});
+        defer std.testing.allocator.free(string);
+        try std.testing.expectEqualStrings(expected, string);
+    }
+}
