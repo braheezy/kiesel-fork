0ee576f1eee8bdfff1b216e1dcc1a3e784e1de39
Linus Groh <mail@linusgroh.de>
2023-05-01 14:51:08 +0100

    types: Implement BigInt.format()

--- src/types/language/BigInt.zig
+++ src/types/language/BigInt.zig
@@ -11,10 +11,38 @@ pub const Value = std.math.big.int.Managed;
 
 value: Value,
 
+pub fn format(
+    self: Self,
+    comptime fmt: []const u8,
+    options: std.fmt.FormatOptions,
+    writer: anytype,
+) !void {
+    _ = fmt;
+    _ = options;
+    try writer.print("{}", .{self.value});
+    try writer.writeAll("n");
+}
+
 /// 6.1.6.2.21 BigInt::toString ( x, radix )
 /// https://tc39.es/ecma262/#sec-numeric-types-bigint-tostring
 pub fn toString(self: Self, allocator: Allocator, radix: u8) ![]const u8 {
     // 1. If x < 0ℤ, return the string-concatenation of "-" and BigInt::toString(-x, radix).
     // 2. Return the String value consisting of the representation of x using radix radix.
     return self.value.toString(allocator, radix, .lower);
+}
+
+test "format" {
+    const test_cases = [_]struct { Self, []const u8 }{
+        .{ .{ .value = try Value.initSet(std.testing.allocator, 0) }, "0n" },
+        .{ .{ .value = try Value.initSet(std.testing.allocator, 123) }, "123n" },
+        .{ .{ .value = try Value.initSet(std.testing.allocator, -42) }, "-42n" },
+    };
+    for (test_cases) |test_case| {
+        const big_int = test_case[0];
+        const expected = test_case[1];
+        const string = try std.fmt.allocPrint(std.testing.allocator, "{}", .{big_int});
+        defer std.testing.allocator.free(string);
+        defer @constCast(&big_int.value).deinit();
+        try std.testing.expectEqualStrings(expected, string);
+    }
 }
