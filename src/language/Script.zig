736769d39a954bae83f2706c8b47bffd7382018b
Linus Groh <mail@linusgroh.de>
2023-05-16 18:02:19 +0100

    language: Implement more of Script.evaluate()

--- src/language/Script.zig
+++ src/language/Script.zig
@@ -11,6 +11,7 @@ const execution = @import("../execution.zig");
 const types = @import("../types.zig");
 
 const Executable = bytecode.Executable;
+const ExecutionContext = execution.ExecutionContext;
 const Parser = @import("Parser.zig");
 const Realm = execution.Realm;
 const Value = types.Value;
@@ -57,21 +58,79 @@ pub fn parse(
 
 /// 16.1.6 ScriptEvaluation ( scriptRecord )
 /// https://tc39.es/ecma262/#sec-runtime-semantics-scriptevaluation
-pub fn evaluate(self: Self) !Value {
+pub fn evaluate(self: *Self) !Value {
     const agent = self.realm.agent;
 
-    var executable = Executable.init(agent.gc_allocator);
-    defer executable.deinit();
+    // 1. Let globalEnv be scriptRecord.[[Realm]].[[GlobalEnv]].
+    const global_env = self.realm.global_env;
 
-    var vm = try Vm.init(agent);
-    defer vm.deinit();
+    // 1. Let scriptContext be a new ECMAScript code execution context.
+    const script_context = ExecutionContext{
+        // 3. Set the Function of scriptContext to null.
+        .function = null,
 
-    try self.ecmascript_code.generateBytecode(&executable);
+        // 4. Set the Realm of scriptContext to scriptRecord.[[Realm]].
+        .realm = self.realm,
 
-    if (agent.options.debug.print_bytecode) {
-        const stdout = std.io.getStdOut().writer();
-        try executable.print(stdout);
-    }
+        // 5. Set the ScriptOrModule of scriptContext to scriptRecord.
+        .script_or_module = .{ .script = self },
 
-    return try vm.run(executable) orelse .undefined;
+        .ecmascript_code = .{
+            // 6. Set the VariableEnvironment of scriptContext to globalEnv.
+            .variable_environment = .{ .global_environment = global_env },
+
+            // 7. Set the LexicalEnvironment of scriptContext to globalEnv.
+            .lexical_environment = .{ .global_environment = global_env },
+
+            // 8. Set the PrivateEnvironment of scriptContext to null.
+            .private_environment = null,
+        },
+    };
+
+    // TODO: 9. Suspend the running execution context.
+
+    // 10. Push scriptContext onto the execution context stack; scriptContext is now the running execution context.
+    try agent.execution_context_stack.append(script_context);
+
+    // 11. Let script be scriptRecord.[[ECMAScriptCode]].
+    const script = self.ecmascript_code;
+
+    // TODO: 12. Let result be Completion(GlobalDeclarationInstantiation(script, globalEnv)).
+    const result_no_value: error{ExceptionThrown}!void = {};
+
+    // 13. If result.[[Type]] is normal, then
+    const result = if (result_no_value) |_| blk: {
+        var vm = try Vm.init(agent);
+        defer vm.deinit();
+
+        var executable = Executable.init(agent.gc_allocator);
+        defer executable.deinit();
+
+        try script.generateBytecode(&executable);
+
+        if (agent.options.debug.print_bytecode) {
+            const stdout = std.io.getStdOut().writer();
+            try executable.print(stdout);
+        }
+
+        // a. Set result to Completion(Evaluation of script).
+        // b. If result.[[Type]] is normal and result.[[Value]] is empty, then
+        if (vm.run(executable)) |value|
+            // i. Set result to NormalCompletion(undefined).
+            break :blk value orelse .undefined
+        else |err|
+            break :blk err;
+    } else |err| err;
+
+    // 14. Suspend scriptContext and remove it from the execution context stack.
+    _ = agent.execution_context_stack.pop();
+
+    // 15. Assert: The execution context stack is not empty.
+    std.debug.assert(agent.execution_context_stack.items.len > 0);
+
+    // TODO: 16. Resume the context that is now on the top of the execution context stack as the
+    //     running execution context.
+
+    // 17. Return ? result.
+    return try result;
 }
