a40f0c46fbd3bd9e4114c7b139222f4734e3d5fa
Linus Groh <mail@linusgroh.de>
2023-05-17 09:34:55 +0100

    builtins: Add globalThis, Infinity,NaN,and undefined

--- src/builtins/global.zig
+++ src/builtins/global.zig
@@ -16,6 +16,22 @@ const NameAndPropertyDescriptor = struct {
 pub fn globalObjectProperties(realm: *Realm) ![5]NameAndPropertyDescriptor {
     // NOTE: For the sake of compactness we're breaking the line length recommendations here.
     return [_]NameAndPropertyDescriptor{
+        // 19.1.1 globalThis
+        // https://tc39.es/ecma262/#sec-globalthis
+        .{ "globalThis", .{ .value = Value.from(realm.global_env.global_this_value), .writable = true, .enumerable = false, .configurable = true } },
+
+        // 19.1.2 Infinity
+        // https://tc39.es/ecma262/#sec-value-properties-of-the-global-object-infinity
+        .{ "Infinity", .{ .value = Value.infinity(), .writable = false, .enumerable = false, .configurable = false } },
+
+        // 19.1.3 NaN
+        // https://tc39.es/ecma262/#sec-value-properties-of-the-global-object-nan
+        .{ "NaN", .{ .value = Value.nan(), .writable = false, .enumerable = false, .configurable = false } },
+
+        // 19.1.4 undefined
+        // https://tc39.es/ecma262/#sec-undefined
+        .{ "undefined", .{ .value = .undefined, .writable = false, .enumerable = false, .configurable = false } },
+
         // 19.3.7 Boolean ( . . . )
         // https://tc39.es/ecma262/#sec-constructor-properties-of-the-global-object-boolean
         .{ "Boolean", .{ .value = Value.from(try realm.intrinsics.@"%Boolean%"()), .writable = true, .enumerable = false, .configurable = true } },
