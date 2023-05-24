e3b0660fc7afe8f050cb551fce123620b1cb0a13
Linus Groh <mail@linusgroh.de>
2023-05-24 17:49:18 +0100

    main: Export the utils namespace

--- src/main.zig
+++ src/main.zig
@@ -2,6 +2,7 @@ pub const builtins = @import("builtins.zig");
 pub const execution = @import("execution.zig");
 pub const language = @import("language.zig");
 pub const types = @import("types.zig");
+pub const utils = @import("utils.zig");
 
 pub const version_string = "0.1.0";
 
@@ -10,4 +11,5 @@ test {
     _ = execution;
     _ = language;
     _ = types;
+    _ = utils;
 }
