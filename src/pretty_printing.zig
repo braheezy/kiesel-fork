braheezy/kiesel-fork

commit b7377df32987032e989057cbd12814461709ce02

builtins: Implement BigInt objects

src/pretty_printing.zig
@@ -63,13 +63,14 @@ fn prettyPrintPrimitiveWrapper(object: Object, writer: anytype) !void {
     const tty_config = getTtyConfigForWriter(writer);
 
     const name = blk: {
-        if (object.is(builtins.Boolean)) break :blk "Boolean";
+        if (object.is(builtins.BigInt)) break :blk "BigInt";
         if (object.is(builtins.Number)) break :blk "Number";
         if (object.is(builtins.String)) break :blk "String";
         if (object.is(builtins.Symbol)) break :blk "Symbol";
         @panic("Unhandled object type in prettyPrintPrimitiveWrapper()");
     };
     const value = blk: {
+        if (object.is(builtins.BigInt)) break :blk Value.from(object.as(builtins.BigInt).fields.big_int_data);
         if (object.is(builtins.Boolean)) break :blk Value.from(object.as(builtins.Boolean).fields.boolean_data);
         if (object.is(builtins.Number)) break :blk Value.from(object.as(builtins.Number).fields.number_data);
         if (object.is(builtins.String)) break :blk Value.from(object.as(builtins.String).fields.string_data);
@@ -170,7 +171,8 @@ pub fn prettyPrintValue(value: Value, writer: anytype) !void {
 
         if (object.is(builtins.Array))
             return prettyPrintArray(object, writer);
-        if (object.is(builtins.Boolean) or
+        if (object.is(builtins.BigInt) or
+            object.is(builtins.Boolean) or
             object.is(builtins.Number) or
             object.is(builtins.String) or
             object.is(builtins.Symbol))

src/builtins.zig
@@ -1,4 +1,5 @@
 const array = @import("builtins/array.zig");
+const big_int = @import("builtins/big_int.zig");
 const boolean = @import("builtins/boolean.zig");
 const builtin_function = @import("builtins/builtin_function.zig");
 const ecmascript_function = @import("builtins/ecmascript_function.zig");
@@ -20,6 +21,9 @@ pub const Array = array.Array;
 pub const ArrayConstructor = array.ArrayConstructor;
 pub const ArrayPrototype = array.ArrayPrototype;
 pub const Behaviour = builtin_function.Behaviour;
+pub const BigInt = big_int.BigInt;
+pub const BigIntConstructor = big_int.BigIntConstructor;
+pub const BigIntPrototype = big_int.BigIntPrototype;
 pub const Boolean = boolean.Boolean;
 pub const BooleanConstructor = boolean.BooleanConstructor;
 pub const BooleanPrototype = boolean.BooleanPrototype;

src/builtins/big_int.zig
@@ -1,0 +1,132 @@
+//! 21.2 BigInt Objects
+//! https://tc39.es/ecma262/#sec-bigint-objects
+
+const builtins = @import("../builtins.zig");
+const execution = @import("../execution.zig");
+const types = @import("../types.zig");
+const utils = @import("../utils.zig");
+
+const Agent = execution.Agent;
+const ArgumentsList = builtins.ArgumentsList;
+const Number = types.Number;
+const Object = types.Object;
+const PropertyDescriptor = types.PropertyDescriptor;
+const Realm = execution.Realm;
+const Value = types.Value;
+const createBuiltinFunction = builtins.createBuiltinFunction;
+const defineBuiltinProperty = utils.defineBuiltinProperty;
+const ordinaryCreateFromConstructor = builtins.ordinaryCreateFromConstructor;
+
+/// 21.2.2 Properties of the BigInt Constructor
+/// https://tc39.es/ecma262/#sec-properties-of-the-bigint-constructor
+pub const BigIntConstructor = struct {
+    pub fn create(realm: *Realm) !Object {
+        const object = try createBuiltinFunction(realm.agent, .{ .constructor = behaviour }, .{
+            .length = 1,
+            .name = "BigInt",
+            .realm = realm,
+            .prototype = try realm.intrinsics.@"%Function.prototype%"(),
+        });
+
+        // 21.2.2.3 BigInt.prototype
+        // https://tc39.es/ecma262/#sec-bigint.prototype
+        try defineBuiltinProperty(object, "prototype", PropertyDescriptor{
+            .value = Value.from(try realm.intrinsics.@"%BigInt.prototype%"()),
+            .writable = false,
+            .enumerable = false,
+            .configurable = false,
+        });
+
+        // 21.2.3.1 BigInt.prototype.constructor
+        // https://tc39.es/ecma262/#sec-bigint.prototype.constructor
+        try defineBuiltinProperty(
+            realm.intrinsics.@"%BigInt.prototype%"() catch unreachable,
+            "constructor",
+            Value.from(object),
+        );
+
+        return object;
+    }
+
+    /// 21.2.1.1 BigInt ( value )
+    /// https://tc39.es/ecma262/#sec-bigint-constructor-number-value
+    fn behaviour(agent: *Agent, _: Value, arguments: ArgumentsList, new_target: ?Object) !Value {
+        const value = arguments.get(0);
+
+        // 1. If NewTarget is not undefined, throw a TypeError exception.
+        if (new_target != null) {
+            return agent.throwException(.type_error, "BigInt is not a constructor");
+        }
+
+        // 2. Let prim be ? ToPrimitive(value, number).
+        const primitive = try value.toPrimitive(agent, .number);
+
+        // 3. If prim is a Number, return ? NumberToBigInt(prim).
+        if (primitive == .number) return Value.from(try numberToBigInt(agent, primitive.number));
+
+        // 4. Otherwise, return ? ToBigInt(prim).
+        return Value.from(try primitive.toBigInt(agent));
+    }
+};
+
+/// 21.2.1.1.1 NumberToBigInt ( number )
+/// https://tc39.es/ecma262/#sec-numbertobigint
+fn numberToBigInt(agent: *Agent, number: Number) !types.BigInt {
+    // 1. If IsIntegralNumber(number) is false, throw a RangeError exception.
+    if (!Value.from(number).isIntegralNumber()) {
+        return agent.throwException(.range_error, "Cannot convert non-integral number to BigInt");
+    }
+
+    // 2. Return ℤ(ℝ(number)).
+    var value = try types.BigInt.Value.init(agent.gc_allocator);
+    value.setString(10, try number.toString(agent.gc_allocator, 10)) catch |err| switch (err) {
+        error.OutOfMemory => return error.OutOfMemory,
+        error.InvalidBase, error.InvalidCharacter => unreachable,
+    };
+    return .{ .value = value };
+}
+
+/// 21.2.3 Properties of the BigInt Prototype Object
+/// https://tc39.es/ecma262/#sec-properties-of-the-bigint-prototype-object
+pub const BigIntPrototype = struct {
+    pub fn create(realm: *Realm) !Object {
+        const object = try builtins.Object.create(realm.agent, .{
+            .prototype = try realm.intrinsics.@"%Object.prototype%"(),
+        });
+
+        return object;
+    }
+
+    /// https://tc39.es/ecma262/#thisbigintvalue
+    fn thisBigIntValue(agent: *Agent, value: Value) !types.BigInt {
+        switch (value) {
+            // 1. If value is a BigInt, return value.
+            .big_int => |big_int| return big_int,
+
+            // 2. If value is an Object and value has a [[BigIntData]] internal slot, then
+            .object => |object| if (object.is(BigInt)) {
+                // a. Assert: value.[[BigIntData]] is a BigInt.
+                // b. Return value.[[BigIntData]].
+                return object.as(BigInt).fields.big_int_data;
+            },
+
+            else => {},
+        }
+
+        // 3. Throw a TypeError exception.
+        return agent.throwException(
+            .type_error,
+            "This value must be a BigInt or BigInt object",
+        );
+    }
+};
+
+/// TODO: The spec has nothing to link for this.
+/// See: https://github.com/tc39/ecma262/issues/3110
+pub const BigInt = Object.Factory(.{
+    .Fields = struct {
+        /// [[BigIntData]]
+        big_int_data: types.BigInt,
+    },
+    .tag = .big_int,
+});

src/builtins/global.zig
@@ -21,7 +21,7 @@ const NameAndPropertyDescriptor = struct {
     PropertyDescriptor,
 };
 
-pub fn globalObjectProperties(realm: *Realm) ![14]NameAndPropertyDescriptor {
+pub fn globalObjectProperties(realm: *Realm) ![15]NameAndPropertyDescriptor {
     // NOTE: For the sake of compactness we're breaking the line length recommendations here.
     return [_]NameAndPropertyDescriptor{
         // 19.1.1 globalThis
@@ -55,6 +55,10 @@ pub fn globalObjectProperties(realm: *Realm) ![14]NameAndPropertyDescriptor {
         // 19.3.2 Array ( . . . )
         // https://tc39.es/ecma262/#sec-constructor-properties-of-the-global-object-array
         .{ "Array", .{ .value = Value.from(try realm.intrinsics.@"%Array%"()), .writable = true, .enumerable = false, .configurable = true } },
+
+        // 19.3.4 BigInt ( . . . )
+        // https://tc39.es/ecma262/#sec-constructor-properties-of-the-global-object-bigint
+        .{ "BigInt", .{ .value = Value.from(try realm.intrinsics.@"%BigInt%"()), .writable = true, .enumerable = false, .configurable = true } },
 
         // 19.3.7 Boolean ( . . . )
         // https://tc39.es/ecma262/#sec-constructor-properties-of-the-global-object-boolean

src/execution/Realm/Intrinsics.zig
@@ -17,6 +17,8 @@ lazy_intrinsics: struct {
     @"%Array.prototype%": ?Object = null,
     @"%Boolean%": ?Object = null,
     @"%Boolean.prototype%": ?Object = null,
+    @"%BigInt%": ?Object = null,
+    @"%BigInt.prototype%": ?Object = null,
     @"%eval%": ?Object = null,
     @"%Function%": ?Object = null,
     @"%Function.prototype%": ?Object = null,
@@ -58,6 +60,12 @@ pub fn @"%Array%"(self: *Self) error{OutOfMemory}!Object {
 }
 pub fn @"%Array.prototype%"(self: *Self) error{OutOfMemory}!Object {
     return self.lazyIntrinsic("%Array.prototype%", builtins.ArrayPrototype);
+}
+pub fn @"%BigInt%"(self: *Self) error{OutOfMemory}!Object {
+    return self.lazyIntrinsic("%BigInt%", builtins.BigIntConstructor);
+}
+pub fn @"%BigInt.prototype%"(self: *Self) error{OutOfMemory}!Object {
+    return self.lazyIntrinsic("%BigInt.prototype%", builtins.BigIntPrototype);
 }
 pub fn @"%Boolean%"(self: *Self) error{OutOfMemory}!Object {
     return self.lazyIntrinsic("%Boolean%", builtins.BooleanConstructor);

src/types/language/Object.zig
@@ -26,6 +26,7 @@ const Self = @This();
 
 pub const Tag = enum(u32) {
     array,
+    big_int,
     boolean,
     builtin_function,
     ecmascript_function,
@@ -60,6 +61,7 @@ pub fn format(
 pub inline fn is(self: Self, comptime T: type) bool {
     inline for ([_]struct { type, Tag }{
         .{ builtins.Array, .array },
+        .{ builtins.BigInt, .big_int },
         .{ builtins.Boolean, .boolean },
         .{ builtins.BuiltinFunction, .builtin_function },
         .{ builtins.ECMAScriptFunction, .ecmascript_function },

src/types/language/value.zig
@@ -581,8 +581,10 @@ pub const Value = union(enum) {
                 .fields = .{ .symbol_data = symbol },
                 .prototype = try realm.intrinsics.@"%Symbol.prototype%"(),
             }),
-            // TODO: Implement these objects
-            .big_int => agent.throwException(.type_error, "toObject() not implemented for BigInt"),
+            .big_int => |big_int| try builtins.BigInt.create(agent, .{
+                .fields = .{ .big_int_data = big_int },
+                .prototype = try realm.intrinsics.@"%BigInt.prototype%"(),
+            }),
             .object => |object| object,
         };
     }

commit b44f10e62487b5895e0d92eff18afbed20dd8b06

pretty_printing: Restore proper handling of Boolean objects
This regressed in b7377df32 where it got replaced with BigInt by
accident.

src/pretty_printing.zig
@@ -64,6 +64,7 @@ fn prettyPrintPrimitiveWrapper(object: Object, writer: anytype) !void {
 
     const name = blk: {
         if (object.is(builtins.BigInt)) break :blk "BigInt";
+        if (object.is(builtins.Boolean)) break :blk "Boolean";
         if (object.is(builtins.Number)) break :blk "Number";
         if (object.is(builtins.String)) break :blk "String";
         if (object.is(builtins.Symbol)) break :blk "Symbol";