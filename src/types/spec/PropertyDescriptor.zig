80d7deb19913d25293f9c7e9e263a3ad7c7af041
Linus Groh <mail@linusgroh.de>
2023-07-12 11:56:58 +0200

    types: Implement PropertyDescriptor.fromPropertyDescriptor()

--- src/types/spec/PropertyDescriptor.zig
+++ src/types/spec/PropertyDescriptor.zig
@@ -3,10 +3,17 @@
 
 const std = @import("std");
 
+const builtins = @import("../../builtins.zig");
+const execution = @import("../../execution.zig");
 const language = @import("../language.zig");
+const utils = @import("../../utils.zig");
 
+const Agent = execution.Agent;
 const Object = language.Object;
+const PropertyKey = language.Object.PropertyKey;
 const Value = language.Value;
+const noexcept = utils.noexcept;
+const ordinaryObjectCreate = builtins.ordinaryObjectCreate;
 
 const Self = @This();
 
@@ -58,6 +65,75 @@ pub inline fn isGenericDescriptor(self: Self) bool {
     return !(self.isAccessorDescriptor() or self.isDataDescriptor());
 }
 
+/// 6.2.6.4 FromPropertyDescriptor ( Desc )
+/// https://tc39.es/ecma262/#sec-frompropertydescriptor
+pub fn fromPropertyDescriptor(self: Self, agent: *Agent) !?Object {
+    const realm = agent.currentRealm();
+
+    // 1. If Desc is undefined, return undefined.
+
+    // 2. Let obj be OrdinaryObjectCreate(%Object.prototype%).
+    // 3. Assert: obj is an extensible ordinary object with no own properties.
+    const object = try ordinaryObjectCreate(agent, try realm.intrinsics.@"%Object.prototype%"());
+
+    // 4. If Desc has a [[Value]] field, then
+    if (self.value) |value| {
+        // a. Perform ! CreateDataPropertyOrThrow(obj, "value", Desc.[[Value]]).
+        object.createDataPropertyOrThrow(
+            PropertyKey.from("value"),
+            value,
+        ) catch |err| try noexcept(err);
+    }
+
+    // 5. If Desc has a [[Writable]] field, then
+    if (self.writable) |writable| {
+        // a. Perform ! CreateDataPropertyOrThrow(obj, "writable", Desc.[[Writable]]).
+        object.createDataPropertyOrThrow(
+            PropertyKey.from("writable"),
+            Value.from(writable),
+        ) catch |err| try noexcept(err);
+    }
+
+    // 6. If Desc has a [[Get]] field, then
+    if (self.get) |get| {
+        // a. Perform ! CreateDataPropertyOrThrow(obj, "get", Desc.[[Get]]).
+        object.createDataPropertyOrThrow(
+            PropertyKey.from("get"),
+            Value.from(get),
+        ) catch |err| try noexcept(err);
+    }
+
+    // 7. If Desc has a [[Set]] field, then
+    if (self.set) |set| {
+        // a. Perform ! CreateDataPropertyOrThrow(obj, "set", Desc.[[Set]]).
+        object.createDataPropertyOrThrow(
+            PropertyKey.from("set"),
+            Value.from(set),
+        ) catch |err| try noexcept(err);
+    }
+
+    // 8. If Desc has an [[Enumerable]] field, then
+    if (self.enumerable) |enumerable| {
+        // a. Perform ! CreateDataPropertyOrThrow(obj, "enumerable", Desc.[[Enumerable]]).
+        object.createDataPropertyOrThrow(
+            PropertyKey.from("enumerable"),
+            Value.from(enumerable),
+        ) catch |err| try noexcept(err);
+    }
+
+    // 9. If Desc has a [[Configurable]] field, then
+    if (self.configurable) |configurable| {
+        // a. Perform ! CreateDataPropertyOrThrow(obj, "configurable", Desc.[[Configurable]]).
+        object.createDataPropertyOrThrow(
+            PropertyKey.from("configurable"),
+            Value.from(configurable),
+        ) catch |err| try noexcept(err);
+    }
+
+    // 10. Return obj.
+    return object;
+}
+
 pub inline fn isFullyPopulated(self: Self) bool {
     return ((self.value != null and self.writable != null) or
         (self.get != null or self.set != null)) and
@@ -75,8 +151,6 @@ pub inline fn hasFields(self: Self) bool {
 }
 
 test "isAccessorDescriptor" {
-    const builtins = @import("../../builtins.zig");
-    const Agent = @import("../../execution.zig").Agent;
     var agent = try Agent.init(.{});
     defer agent.deinit();
     const getter = try builtins.Object.create(&agent, .{
@@ -100,8 +174,6 @@ test "isDataDescriptor" {
 }
 
 test "isGenericDescriptor" {
-    const builtins = @import("../../builtins.zig");
-    const Agent = @import("../../execution.zig").Agent;
     var agent = try Agent.init(.{});
     defer agent.deinit();
     const setter = try builtins.Object.create(&agent, .{
