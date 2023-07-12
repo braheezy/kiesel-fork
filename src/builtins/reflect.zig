156f3d90e1a0510b09a6837e468648460b0871fc
Linus Groh <mail@linusgroh.de>
2023-07-12 13:33:57 +0200

    builtins: Implement Reflect.ownKeys()

--- src/builtins/reflect.zig
+++ src/builtins/reflect.zig
@@ -13,6 +13,7 @@ const ArgumentsList = builtins.ArgumentsList;
 const Object = types.Object;
 const Realm = execution.Realm;
 const Value = types.Value;
+const createArrayFromList = types.createArrayFromList;
 const defineBuiltinFunction = utils.defineBuiltinFunction;
 
 pub const Reflect = struct {
@@ -30,6 +31,7 @@ pub const Reflect = struct {
         try defineBuiltinFunction(object, "getPrototypeOf", getPrototypeOf, 1, realm);
         try defineBuiltinFunction(object, "has", has, 2, realm);
         try defineBuiltinFunction(object, "isExtensible", isExtensible, 1, realm);
+        try defineBuiltinFunction(object, "ownKeys", ownKeys, 1, realm);
 
         return object;
     }
@@ -252,5 +254,38 @@ pub const Reflect = struct {
 
         // 2. Return ? target.[[IsExtensible]]().
         return Value.from(try target.object.internalMethods().isExtensible(target.object));
+    }
+
+    /// 28.1.10 Reflect.ownKeys ( target )
+    /// https://tc39.es/ecma262/#sec-reflect.ownkeys
+    fn ownKeys(agent: *Agent, _: Value, arguments: ArgumentsList) !Value {
+        const target = arguments.get(0);
+
+        // 1. If target is not an Object, throw a TypeError exception.
+        if (target != .object) {
+            return agent.throwException(
+                .type_error,
+                try std.fmt.allocPrint(agent.gc_allocator, "{} is not an Object", .{target}),
+            );
+        }
+
+        // 2. Let keys be ? target.[[OwnPropertyKeys]]().
+        const property_keys = try target.object.internalMethods().ownPropertyKeys(target.object);
+        // TODO: Add createArrayFromList() overload for type conversions
+        var keys = try std.ArrayList(Value).initCapacity(agent.gc_allocator, property_keys.items.len);
+        for (property_keys.items) |property_key| {
+            keys.appendAssumeCapacity(switch (property_key) {
+                .string => |string| Value.from(string),
+                .symbol => |symbol| Value.from(symbol),
+                .integer_index => |integer_index| Value.from(try std.fmt.allocPrint(
+                    agent.gc_allocator,
+                    "{}",
+                    .{integer_index},
+                )),
+            });
+        }
+
+        // 3. Return CreateArrayFromList(keys).
+        return Value.from(try createArrayFromList(agent, keys.items));
     }
 };
