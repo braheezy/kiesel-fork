e86b178158d6a49558d7d1e9706dc647ef6adf71
Linus Groh <mail@linusgroh.de>
2023-07-20 18:07:14 +0100

    builtins: Implement Array.prototype.map()

--- src/builtins/array.zig
+++ src/builtins/array.zig
@@ -553,6 +553,7 @@ pub const ArrayPrototype = struct {
 
         try defineBuiltinFunction(object, "forEach", forEach, 1, realm);
         try defineBuiltinFunction(object, "join", join, 1, realm);
+        try defineBuiltinFunction(object, "map", map, 1, realm);
         try defineBuiltinFunction(object, "pop", pop, 0, realm);
         try defineBuiltinFunction(object, "push", push, 1, realm);
         try defineBuiltinFunction(object, "toString", toString, 0, realm);
@@ -657,6 +658,63 @@ pub const ArrayPrototype = struct {
         return Value.from(
             try std.mem.join(agent.gc_allocator, sep, elements.items),
         );
+    }
+
+    /// 23.1.3.21 Array.prototype.map ( callbackfn [ , thisArg ] )
+    /// https://tc39.es/ecma262/#sec-array.prototype.map
+    fn map(agent: *Agent, this_value: Value, arguments: ArgumentsList) !Value {
+        const callback_fn = arguments.get(0);
+        const this_arg = arguments.get(1);
+
+        // 1. Let O be ? ToObject(this value).
+        const object = try this_value.toObject(agent);
+
+        // 2. Let len be ? LengthOfArrayLike(O).
+        const len = try object.lengthOfArrayLike();
+
+        // 3. If IsCallable(callbackfn) is false, throw a TypeError exception.
+        if (!callback_fn.isCallable()) {
+            return agent.throwException(
+                .type_error,
+                try std.fmt.allocPrint(agent.gc_allocator, "{} is not callable", .{callback_fn}),
+            );
+        }
+
+        // 4. Let A be ? ArraySpeciesCreate(O, len).
+        if (len > std.math.maxInt(usize)) return error.OutOfMemory;
+        const array = try arraySpeciesCreate(agent, object, @intCast(len));
+
+        // 5. Let k be 0.
+        var k: u53 = 0;
+
+        // 6. Repeat, while k < len,
+        while (k < len) : (k += 1) {
+            // a. Let Pk be ! ToString(𝔽(k)).
+            const property_key = PropertyKey.from(k);
+
+            // b. Let kPresent be ? HasProperty(O, Pk).
+            const k_present = try object.hasProperty(property_key);
+
+            // c. If kPresent is true, then
+            if (k_present) {
+                // i. Let kValue be ? Get(O, Pk).
+                const k_value = try object.get(property_key);
+
+                // ii. Let mappedValue be ? Call(callbackfn, thisArg, « kValue, 𝔽(k), O »).
+                const mapped_value = try callback_fn.callAssumeCallable(
+                    this_arg,
+                    .{ k_value, Value.from(k), Value.from(object) },
+                );
+
+                // iii. Perform ? CreateDataPropertyOrThrow(A, Pk, mappedValue).
+                try array.createDataPropertyOrThrow(property_key, mapped_value);
+            }
+
+            // d. Set k to k + 1.
+        }
+
+        // 7. Return A.
+        return Value.from(array);
     }
 
     /// 23.1.3.23 Array.prototype.push ( ...items )
