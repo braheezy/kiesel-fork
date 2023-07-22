c9e673a7e00b43bb5c9bcc1370d54bd45330bdb8
Linus Groh <mail@linusgroh.de>
2023-07-22 09:35:48 +0200

    builtins: Implement Number.NaN

--- src/builtins/number.zig
+++ src/builtins/number.zig
@@ -75,6 +75,15 @@ pub const NumberConstructor = struct {
             .configurable = false,
         });
 
+        // 21.1.2.10 Number.NaN
+        // https://tc39.es/ecma262/#sec-number.nan
+        try defineBuiltinProperty(object, "NaN", PropertyDescriptor{
+            .value = Value.nan(),
+            .writable = false,
+            .enumerable = false,
+            .configurable = false,
+        });
+
         try defineBuiltinFunction(object, "isFinite", isFinite, 1, realm);
         try defineBuiltinFunction(object, "isInteger", isInteger, 1, realm);
         try defineBuiltinFunction(object, "isNaN", isNaN, 1, realm);
