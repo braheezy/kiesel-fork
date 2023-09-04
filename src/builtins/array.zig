ea5c4719c2616fbb2f43ffcc50e170b562392577
Linus Groh <mail@linusgroh.de>
2023-09-04 13:53:27 +0200

    builtins: Implement Array.prototype.indexOf()

--- src/builtins/array.zig
+++ src/builtins/array.zig
@@ -22,6 +22,7 @@ const defineBuiltinAccessor = utils.defineBuiltinAccessor;
 const defineBuiltinFunction = utils.defineBuiltinFunction;
 const defineBuiltinProperty = utils.defineBuiltinProperty;
 const getPrototypeFromConstructor = builtins.getPrototypeFromConstructor;
+const isStrictlyEqual = types.isStrictlyEqual;
 const noexcept = utils.noexcept;
 const ordinaryDefineOwnProperty = ordinary.ordinaryDefineOwnProperty;
 const ordinaryGetOwnProperty = ordinary.ordinaryGetOwnProperty;
@@ -552,6 +553,7 @@ pub const ArrayPrototype = struct {
 
         try defineBuiltinFunction(object, "forEach", forEach, 1, realm);
         try defineBuiltinFunction(object, "includes", includes, 1, realm);
+        try defineBuiltinFunction(object, "indexOf", indexOf, 1, realm);
         try defineBuiltinFunction(object, "join", join, 1, realm);
         try defineBuiltinFunction(object, "map", map, 1, realm);
         try defineBuiltinFunction(object, "pop", pop, 0, realm);
@@ -703,6 +705,63 @@ pub const ArrayPrototype = struct {
 
         // 11. Return false.
         return Value.from(false);
+    }
+
+    /// 23.1.3.17 Array.prototype.indexOf ( searchElement [ , fromIndex ] )
+    /// https://tc39.es/ecma262/#sec-array.prototype.indexof
+    fn indexOf(agent: *Agent, this_value: Value, arguments: ArgumentsList) !Value {
+        const search_element = arguments.get(0);
+        const from_index = arguments.get(1);
+
+        // 1. Let O be ? ToObject(this value).
+        const object = try this_value.toObject(agent);
+
+        // 2. Let len be ? LengthOfArrayLike(O).
+        const len = try object.lengthOfArrayLike();
+
+        // 3. If len = 0, return -1𝔽.
+        if (len == 0) return Value.from(-1);
+
+        // 4. Let n be ? ToIntegerOrInfinity(fromIndex).
+        var n = try from_index.toIntegerOrInfinity(agent);
+
+        // 5. Assert: If fromIndex is undefined, then n is 0.
+        if (from_index == .undefined) std.debug.assert(n == 0);
+
+        // 6. If n = +∞, return -1𝔽.
+        if (n == std.math.inf(f64)) return Value.from(-1);
+
+        // 7. Else if n = -∞, set n to 0.
+        if (n == -std.math.inf(f64)) n = 0;
+
+        // 8. If n ≥ 0, then
+        //     a. Let k be n.
+        // 9. Else,
+        //     a. Let k be len + n.
+        //     b. If k < 0, set k to 0.
+        const k_f64 = if (n >= 0) n else @max(@as(f64, @floatFromInt(len)) + n, 0);
+        if (k_f64 >= std.math.maxInt(u53)) return Value.from(-1);
+        var k: u53 = @intFromFloat(k_f64);
+
+        // 10. Repeat, while k < len,
+        while (k < len) : (k += 1) {
+            // a. Let kPresent be ? HasProperty(O, ! ToString(𝔽(k))).
+            const k_present = try object.hasProperty(PropertyKey.from(k));
+
+            // b. If kPresent is true, then
+            if (k_present) {
+                // i. Let elementK be ? Get(O, ! ToString(𝔽(k))).
+                const element_k = try object.get(PropertyKey.from(k));
+
+                // ii. If IsStrictlyEqual(searchElement, elementK) is true, return 𝔽(k).
+                if (isStrictlyEqual(search_element, element_k)) return Value.from(k);
+            }
+
+            // c. Set k to k + 1.
+        }
+
+        // 11. Return -1𝔽.
+        return Value.from(-1);
     }
 
     /// 23.1.3.18 Array.prototype.join ( separator )
