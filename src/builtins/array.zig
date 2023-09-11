e92df6d71969d6843bc492c6f69874652abc6b7d
Linus Groh <mail@linusgroh.de>
2023-09-11 22:33:43 +0100

    builtins: Implement Array.prototype.entries()

--- src/builtins/array.zig
+++ src/builtins/array.zig
@@ -18,6 +18,7 @@ const PropertyKey = Object.PropertyKey;
 const Realm = execution.Realm;
 const String = types.String;
 const Value = types.Value;
+const createArrayIterator = builtins.createArrayIterator;
 const createBuiltinFunction = builtins.createBuiltinFunction;
 const defineBuiltinAccessor = utils.defineBuiltinAccessor;
 const defineBuiltinFunction = utils.defineBuiltinFunction;
@@ -719,6 +720,7 @@ pub const ArrayPrototype = struct {
         });
 
         try defineBuiltinFunction(object, "at", at, 1, realm);
+        try defineBuiltinFunction(object, "entries", entries, 0, realm);
         try defineBuiltinFunction(object, "every", every, 1, realm);
         try defineBuiltinFunction(object, "find", find, 1, realm);
         try defineBuiltinFunction(object, "findIndex", findIndex, 1, realm);
@@ -810,6 +812,16 @@ pub const ArrayPrototype = struct {
 
         // 7. Return ? Get(O, ! ToString(𝔽(k))).
         return object.get(PropertyKey.from(k));
+    }
+
+    /// 23.1.3.5 Array.prototype.entries ( )
+    /// https://tc39.es/ecma262/#sec-array.prototype.entries
+    fn entries(agent: *Agent, this_value: Value, _: ArgumentsList) !Value {
+        // 1. Let O be ? ToObject(this value).
+        const object = try this_value.toObject(agent);
+
+        // 2. Return CreateArrayIterator(O, key+value).
+        return Value.from(try createArrayIterator(agent, object, .@"key+value"));
     }
 
     /// 23.1.3.6 Array.prototype.every ( callbackfn [ , thisArg ] )
