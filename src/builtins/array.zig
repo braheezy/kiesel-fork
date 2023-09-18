169513d3e67acd02a215d2d0bb143aa325094b59
Linus Groh <mail@linusgroh.de>
2023-09-18 19:46:41 +0200

    builtins: Implement Array.prototype.toReversed()

--- src/builtins/array.zig
+++ src/builtins/array.zig
@@ -743,6 +743,7 @@ pub const ArrayPrototype = struct {
         try defineBuiltinFunction(object, "slice", slice, 2, realm);
         try defineBuiltinFunction(object, "some", some, 1, realm);
         try defineBuiltinFunction(object, "toLocaleString", toLocaleString, 0, realm);
+        try defineBuiltinFunction(object, "toReversed", toReversed, 0, realm);
         try defineBuiltinFunction(object, "toString", toString, 0, realm);
         try defineBuiltinFunction(object, "unshift", unshift, 1, realm);
         try defineBuiltinFunction(object, "values", values, 0, realm);
@@ -2276,6 +2277,42 @@ pub const ArrayPrototype = struct {
         return Value.from(
             try std.mem.join(agent.gc_allocator, separator, elements.items),
         );
+    }
+
+    /// 23.1.3.33 Array.prototype.toReversed ( )
+    /// https://tc39.es/ecma262/#sec-array.prototype.toreversed
+    fn toReversed(agent: *Agent, this_value: Value, _: ArgumentsList) !Value {
+        // 1. Let O be ? ToObject(this value).
+        const object = try this_value.toObject(agent);
+
+        // 2. Let len be ? LengthOfArrayLike(O).
+        const len = try object.lengthOfArrayLike();
+
+        // 3. Let A be ? ArrayCreate(len).
+        const array = try arrayCreate(agent, len, null);
+
+        // 4. Let k be 0.
+        var k: u53 = 0;
+
+        // 5. Repeat, while k < len,
+        while (k < len) : (k += 1) {
+            // a. Let from be ! ToString(𝔽(len - k - 1)).
+            const from = PropertyKey.from(len - k - 1);
+
+            // b. Let Pk be ! ToString(𝔽(k)).
+            const property_key = PropertyKey.from(k);
+
+            // c. Let fromValue be ? Get(O, from).
+            const from_value = try object.get(from);
+
+            // d. Perform ! CreateDataPropertyOrThrow(A, Pk, fromValue).
+            try array.createDataPropertyOrThrow(property_key, from_value);
+
+            // e. Set k to k + 1.
+        }
+
+        // 6. Return A.
+        return Value.from(array);
     }
 
     /// 23.1.3.36 Array.prototype.toString ( )
