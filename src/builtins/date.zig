c6bdc6761e7692290b4979664639415de8492fe3
Linus Groh <mail@linusgroh.de>
2023-09-21 18:05:45 +0100

    builtins: Implement Date.prototype.getUTCHours()

--- src/builtins/date.zig
+++ src/builtins/date.zig
@@ -801,6 +801,7 @@ pub const DatePrototype = struct {
         try defineBuiltinFunction(object, "getUTCDate", getUTCDate, 0, realm);
         try defineBuiltinFunction(object, "getUTCDay", getUTCDay, 0, realm);
         try defineBuiltinFunction(object, "getUTCFullYear", getUTCFullYear, 0, realm);
+        try defineBuiltinFunction(object, "getUTCHours", getUTCHours, 0, realm);
         try defineBuiltinFunction(object, "toDateString", toDateString_, 0, realm);
         try defineBuiltinFunction(object, "toISOString", toISOString, 0, realm);
         try defineBuiltinFunction(object, "toJSON", toJSON, 1, realm);
@@ -1033,6 +1034,23 @@ pub const DatePrototype = struct {
 
         // 5. Return YearFromTime(t).
         return Value.from(yearFromTime(time_value));
+    }
+
+    /// 21.4.4.15 Date.prototype.getUTCHours ( )
+    /// https://tc39.es/ecma262/#sec-date.prototype.getutchours
+    fn getUTCHours(agent: *Agent, this_value: Value, _: ArgumentsList) !Value {
+        // 1. Let dateObject be the this value.
+        // 2. Perform ? RequireInternalSlot(dateObject, [[DateValue]]).
+        const date_object = try this_value.requireInternalSlot(agent, Date);
+
+        // 3. Let t be dateObject.[[DateValue]].
+        const time_value = date_object.fields.date_value;
+
+        // 4. If t is NaN, return NaN.
+        if (std.math.isNan(time_value)) return Value.nan();
+
+        // 5. Return HourFromTime(t).
+        return Value.from(hourFromTime(time_value));
     }
 
     /// 21.4.4.35 Date.prototype.toDateString ( )
