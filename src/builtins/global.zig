4150361b46f71b52a6d9f78edaff03612af74f51
Linus Groh <mail@linusgroh.de>
2023-10-04 23:13:01 +0100

    builtins: Implement parseInt()

--- src/builtins/global.zig
+++ src/builtins/global.zig
@@ -1,9 +1,13 @@
 //! 19 The Global Object
 //! https://tc39.es/ecma262/#sec-global-object
 
+const std = @import("std");
+
 const builtins = @import("../builtins.zig");
 const execution = @import("../execution.zig");
+const tokenizer = @import("../language/tokenizer.zig");
 const types = @import("../types.zig");
+const utils = @import("../utils.zig");
 
 const Agent = execution.Agent;
 const ArgumentsList = builtins.ArgumentsList;
@@ -13,6 +17,9 @@ const Realm = execution.Realm;
 const Value = types.Value;
 const createBuiltinFunction = builtins.createBuiltinFunction;
 const performEval = @import("eval.zig").performEval;
+const trimLeft = utils.trimLeft;
+const line_terminators = tokenizer.line_terminators;
+const whitespace = tokenizer.whitespace;
 
 const Self = @This();
 
@@ -21,7 +28,7 @@ const NameAndPropertyDescriptor = struct {
     PropertyDescriptor,
 };
 
-pub fn globalObjectProperties(realm: *Realm) ![31]NameAndPropertyDescriptor {
+pub fn globalObjectProperties(realm: *Realm) ![32]NameAndPropertyDescriptor {
     // NOTE: For the sake of compactness we're breaking the line length recommendations here.
     return [_]NameAndPropertyDescriptor{
         // 19.1.1 globalThis
@@ -51,6 +58,10 @@ pub fn globalObjectProperties(realm: *Realm) ![31]NameAndPropertyDescriptor {
         // 19.2.3 isNaN ( number )
         // https://tc39.es/ecma262/#sec-isnan-number
         .{ "isNaN", .{ .value = Value.from(try realm.intrinsics.@"%isNaN%"()), .writable = true, .enumerable = false, .configurable = true } },
+
+        // 19.2.5 parseInt ( string, radix )
+        // https://tc39.es/ecma262/#sec-parseint-string-radix
+        .{ "parseInt", .{ .value = Value.from(try realm.intrinsics.@"%parseInt%"()), .writable = true, .enumerable = false, .configurable = true } },
 
         // 19.3.1 AggregateError ( . . . )
         // https://tc39.es/ecma262/#sec-constructor-properties-of-the-global-object-aggregate-error
@@ -166,6 +177,7 @@ pub const global_functions = struct {
     pub const Eval = GlobalFunction(.{ .name = "eval", .length = 1 });
     pub const IsFinite = GlobalFunction(.{ .name = "isFinite", .length = 1 });
     pub const IsNaN = GlobalFunction(.{ .name = "isNaN", .length = 1 });
+    pub const ParseInt = GlobalFunction(.{ .name = "parseInt", .length = 2 });
 };
 
 /// 19.2.1 eval ( x )
@@ -201,4 +213,89 @@ fn isNaN(agent: *Agent, _: Value, arguments: ArgumentsList) !Value {
     // 2. If num is NaN, return true.
     // 3. Otherwise, return false.
     return Value.from(num.isNan());
+}
+
+/// 19.2.5 parseInt ( string, radix )
+/// https://tc39.es/ecma262/#sec-parseint-string-radix
+fn parseInt(agent: *Agent, _: Value, arguments: ArgumentsList) !Value {
+    const string_value = arguments.get(0);
+    const radix_value = arguments.get(1);
+
+    // 1. Let inputString be ? ToString(string).
+    const input_string = try string_value.toString(agent);
+
+    // 2. Let S be ! TrimString(inputString, start).
+    var string = trimLeft(input_string.utf8, &(whitespace ++ line_terminators));
+
+    // 3. Let sign be 1.
+    var sign: f64 = 1;
+
+    // 4. If S is not empty and the first code unit of S is the code unit 0x002D (HYPHEN-MINUS),
+    //    set sign to -1.
+    if (std.mem.startsWith(u8, string, "-")) sign = -1;
+
+    // 5. If S is not empty and the first code unit of S is either the code unit 0x002B (PLUS SIGN)
+    //    or the code unit 0x002D (HYPHEN-MINUS), set S to the substring of S from index 1.
+    if (std.mem.startsWith(u8, string, "+") or std.mem.startsWith(u8, string, "-")) {
+        string = string[1..];
+    }
+
+    // 6. Let R be ℝ(? ToInt32(radix)).
+    var radix = try radix_value.toInt32(agent);
+
+    // 7. Let stripPrefix be true.
+    var strip_prefix = true;
+
+    // 8. If R ≠ 0, then
+    if (radix != 0) {
+        // a. If R < 2 or R > 36, return NaN.
+        if (radix < 2 or radix > 36) return Value.nan();
+
+        // b. If R ≠ 16, set stripPrefix to false.
+        if (radix != 16) strip_prefix = false;
+    }
+    // 9. Else,
+    else {
+        // a. Set R to 10.
+        radix = 10;
+    }
+
+    // 10. If stripPrefix is true, then
+    if (strip_prefix) {
+        // a. If the length of S is at least 2 and the first two code units of S are either "0x" or
+        //    "0X", then
+        if (std.mem.startsWith(u8, string, "0x") or std.mem.startsWith(u8, string, "0X")) {
+            // i. Set S to the substring of S from index 2.
+            string = string[2..];
+
+            // ii. Set R to 16.
+            radix = 16;
+        }
+    }
+
+    // 11. If S contains a code unit that is not a radix-R digit, let end be the index within S of
+    //     the first such code unit; otherwise, let end be the length of S.
+    // 12. Let Z be the substring of S from 0 to end.
+    // 13. If Z is empty, return NaN.
+    // 14. Let mathInt be the integer value that is represented by Z in radix-R notation, using the
+    //     letters A through Z and a through z for digits with values 10 through 35. (However, if
+    //     R = 10 and Z contains more than 20 significant digits, every significant digit after the
+    //     20th may be replaced by a 0 digit, at the option of the implementation; and if R is not
+    //     one of 2, 4, 8, 10, 16, or 32, then mathInt may be an implementation-approximated integer
+    //     representing the integer value denoted by Z in radix-R notation.)
+    var math_int: ?f64 = null;
+    for (string) |c| {
+        const digit = std.fmt.charToDigit(c, @intCast(radix)) catch break;
+        if (math_int == null) math_int = 0;
+        math_int.? *= @floatFromInt(radix);
+        math_int.? += @floatFromInt(digit);
+    }
+
+    if (math_int == null) return Value.nan();
+
+    // 15. If mathInt = 0, then
+    //     a. If sign = -1, return -0𝔽.
+    //     b. Return +0𝔽.
+    // 16. Return 𝔽(sign × mathInt).
+    return Value.from(sign * math_int.?);
 }
--- src/execution/Realm/Intrinsics.zig
+++ src/execution/Realm/Intrinsics.zig
@@ -56,6 +56,7 @@ lazy_intrinsics: struct {
     @"%Object%": ?Object = null,
     @"%Object.prototype%": ?Object = null,
     @"%Object.prototype.toString%": ?Object = null,
+    @"%parseInt%": ?Object = null,
     @"%Promise%": ?Object = null,
     @"%Promise.prototype%": ?Object = null,
     @"%Proxy%": ?Object = null,
@@ -238,6 +239,9 @@ pub fn @"%Object.prototype.toString%"(self: *Self) error{OutOfMemory}!Object {
         intrinsic.* = property_descriptor.?.value.?.object;
     }
     return intrinsic.*.?;
+}
+pub fn @"%parseInt%"(self: *Self) error{OutOfMemory}!Object {
+    return self.lazyIntrinsic("%parseInt%", builtins.global_functions.ParseInt);
 }
 pub fn @"%Promise%"(self: *Self) error{OutOfMemory}!Object {
     return self.lazyIntrinsic("%Promise%", builtins.PromiseConstructor);
