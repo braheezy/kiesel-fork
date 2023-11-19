8fc2e6de882a857e0b222ec16d4a7a85335c069a
Linus Groh <mail@linusgroh.de>
2023-11-19 16:36:15 +0000

    builtins: Implement Intl[@@toStringTag]

--- src/builtins/intl.zig
+++ src/builtins/intl.zig
@@ -8,9 +8,13 @@ const Allocator = std.mem.Allocator;
 const builtins = @import("../builtins.zig");
 const execution = @import("../execution.zig");
 const types = @import("../types.zig");
+const utils = @import("../utils.zig");
 
 const Object = types.Object;
+const PropertyDescriptor = types.PropertyDescriptor;
 const Realm = execution.Realm;
+const Value = types.Value;
+const defineBuiltinProperty = utils.defineBuiltinProperty;
 
 comptime {
     const build_options = @import("build-options");
@@ -21,6 +25,15 @@ pub const Intl = struct {
     pub fn create(realm: *Realm) Allocator.Error!Object {
         const object = try builtins.Object.create(realm.agent, .{
             .prototype = try realm.intrinsics.@"%Object.prototype%"(),
+        });
+
+        // 8.1.1 Intl[ @@toStringTag ]
+        // https://tc39.es/ecma402/#sec-Intl-toStringTag
+        try defineBuiltinProperty(object, "@@toStringTag", PropertyDescriptor{
+            .value = Value.from("Intl"),
+            .writable = false,
+            .enumerable = false,
+            .configurable = true,
         });
 
         return object;
