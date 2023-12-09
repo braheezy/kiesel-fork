fd95c304452f6df7e07f6c78990a4d36b3e9a060
Linus Groh <mail@linusgroh.de>
2023-12-09 19:40:02 +0000

    builtins: Fix redundant inline keyword in comptime scope
    See: https://github.com/ziglang/zig/pull/18227

--- src/builtins/typed_array.zig
+++ src/builtins/typed_array.zig
@@ -2833,7 +2833,7 @@ fn MakeTypedArrayConstructor(comptime name: []const u8) type {
             // 23.2.6.1 TypedArray.BYTES_PER_ELEMENT
             // https://tc39.es/ecma262/#sec-typedarray.bytes_per_element
             try defineBuiltinProperty(object, "BYTES_PER_ELEMENT", PropertyDescriptor{
-                .value = Value.from(comptime inline for (typed_array_element_types) |entry| {
+                .value = Value.from(comptime for (typed_array_element_types) |entry| {
                     const name_, const T = entry;
                     if (std.mem.eql(u8, name, name_)) break @sizeOf(T);
                 }),
@@ -3020,7 +3020,7 @@ fn MakeTypedArrayPrototype(comptime name: []const u8) type {
             // 23.2.7.1 TypedArray.prototype.BYTES_PER_ELEMENT
             // https://tc39.es/ecma262/#sec-typedarray.prototype.bytes_per_element
             try defineBuiltinProperty(object, "BYTES_PER_ELEMENT", PropertyDescriptor{
-                .value = Value.from(comptime inline for (typed_array_element_types) |entry| {
+                .value = Value.from(comptime for (typed_array_element_types) |entry| {
                     const name_, const T = entry;
                     if (std.mem.eql(u8, name, name_)) break @sizeOf(T);
                 }),
