ab2bfedbf954d6be8a7bb57c842071d871f39886
Linus Groh <mail@linusgroh.de>
2023-12-09 19:12:36 +0000

    builtins: Begin implementing evalDeclarationInstantiation()

--- src/builtins/eval.zig
+++ src/builtins/eval.zig
@@ -17,6 +17,7 @@ const Value = types.Value;
 const formatParseError = utils.formatParseError;
 const generateAndRunBytecode = bytecode.generateAndRunBytecode;
 const newDeclarativeEnvironment = execution.newDeclarativeEnvironment;
+const noexcept = utils.noexcept;
 
 /// 19.2.1.1 PerformEval ( x, strictCaller, direct )
 /// https://tc39.es/ecma262/#sec-performeval
@@ -147,8 +148,15 @@ pub fn performEval(agent: *Agent, x: Value, strict_caller: bool, direct: bool) A
     //     execution context.
     try agent.execution_context_stack.append(eval_context);
 
-    // TODO: 28. Let result be Completion(EvalDeclarationInstantiation(body, varEnv, lexEnv, privateEnv, strictEval)).
-    const result_no_value: error{ExceptionThrown}!void = {};
+    // 28. Let result be Completion(EvalDeclarationInstantiation(body, varEnv, lexEnv, privateEnv, strictEval)).
+    const result_no_value = evalDeclarationInstantiation(
+        agent,
+        script,
+        variable_environment,
+        lexical_environment,
+        private_environment,
+        strict_eval,
+    );
 
     // 29. If result.[[Type]] is normal, then
     const result = if (result_no_value) |_| blk: {
@@ -169,4 +177,110 @@ pub fn performEval(agent: *Agent, x: Value, strict_caller: bool, direct: bool) A
 
     // 33. Return ? result.
     return result;
+}
+
+/// 19.2.1.3 EvalDeclarationInstantiation ( body, varEnv, lexEnv, privateEnv, strict )
+/// https://tc39.es/ecma262/#sec-evaldeclarationinstantiation
+fn evalDeclarationInstantiation(
+    agent: *Agent,
+    body: ast.Script,
+    var_env: Environment,
+    lex_env: Environment,
+    private_env: ?*PrivateEnvironment,
+    strict: bool,
+) Agent.Error!void {
+    _ = lex_env;
+    _ = private_env;
+
+    // TODO: 1. Let varNames be the VarDeclaredNames of body.
+
+    // 2. Let varDeclarations be the VarScopedDeclarations of body.
+    const var_declarations = try body.statement_list.varScopedDeclarations(agent.gc_allocator);
+
+    // 3. If strict is false, then
+    if (!strict) {
+        // a. If varEnv is a Global Environment Record, then
+        if (var_env == .global_environment) {
+            // TODO: i. For each element name of varNames, do
+            for (var_declarations) |var_declaration| {
+                const name = var_declaration.binding_identifier;
+
+                // 1. If varEnv.HasLexicalDeclaration(name) is true, throw a SyntaxError exception.
+                if (var_env.global_environment.hasLexicalDeclaration(name)) {
+                    return agent.throwException(
+                        .syntax_error,
+                        "Global environment already has a lexical declaration '{s}'",
+                        .{name},
+                    );
+                }
+
+                // 2. NOTE: eval will not create a global var declaration that would be shadowed by
+                //    a global lexical declaration.
+            }
+        }
+
+        // TODO: b-c.
+    }
+
+    // TODO: 4-10.
+
+    // 11. Let declaredVarNames be a new empty List.
+    var declared_var_names = std.StringHashMap(void).init(agent.gc_allocator);
+    defer declared_var_names.deinit();
+
+    // 12. For each element d of varDeclarations, do
+    for (var_declarations) |var_declaration| {
+        // a. If d is either a VariableDeclaration, a ForBinding, or a BindingIdentifier, then
+        //     i. For each String vn of the BoundNames of d, do
+        for ([_]ast.Identifier{var_declaration.binding_identifier}) |var_name| {
+            // TODO: 1. If declaredFunctionNames does not contain vn, then
+            // a. If varEnv is a Global Environment Record, then
+            if (var_env == .global_environment) {
+                // TODO: i. Let vnDefinable be ? varEnv.CanDeclareGlobalVar(vn).
+                // TODO: ii. If vnDefinable is false, throw a TypeError exception.
+            }
+
+            // b. If declaredVarNames does not contain vn, then
+            if (!declared_var_names.contains(var_name)) {
+                // i. Append vn to declaredVarNames.
+                try declared_var_names.putNoClobber(var_name, {});
+            }
+        }
+    }
+
+    // 13. NOTE: Annex B.3.2.3 adds additional steps at this point.
+    // 14. NOTE: No abnormal terminations occur after this algorithm step unless varEnv is a Global
+    //     Environment Record and the global object is a Proxy exotic object.
+
+    // TODO: 15-17.
+
+    // 18. For each String vn of declaredVarNames, do
+    var it = declared_var_names.keyIterator();
+    while (it.next()) |ptr| {
+        const var_name = ptr.*;
+
+        // a. If varEnv is a Global Environment Record, then
+        if (var_env == .global_environment) {
+            // i. Perform ? varEnv.CreateGlobalVarBinding(vn, true).
+            try var_env.global_environment.createGlobalVarBinding(agent, var_name, true);
+        }
+        // b. Else,
+        else {
+            // i. Let bindingExists be ! varEnv.HasBinding(vn).
+            const binding_exists = var_env.hasBinding(var_name) catch |err| try noexcept(err);
+
+            // ii. If bindingExists is false, then
+            if (!binding_exists) {
+                // 1. NOTE: The following invocation cannot return an abrupt completion because of
+                //    the validation preceding step 14.
+                // 2. Perform ! varEnv.CreateMutableBinding(vn, true).
+                var_env.createMutableBinding(agent, var_name, true) catch |err| try noexcept(err);
+
+                // 3. Perform ! varEnv.InitializeBinding(vn, undefined).
+                var_env.initializeBinding(agent, var_name, .undefined) catch |err| try noexcept(err);
+            }
+        }
+    }
+
+    // 19. Return unused.
 }
