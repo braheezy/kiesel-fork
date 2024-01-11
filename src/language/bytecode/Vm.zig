braheezy/kiesel-fork

commit c7996832edaac48e782f304937007d28b14ca31a

language: Implement ClassBody.nonConstructorElements()
This ensures that a class instance's "constructor" property isn't being
overwritten with the constructor method.

src/language/bytecode/Vm.zig
@@ -1537,8 +1537,9 @@ fn classDefinitionEvaluation(
     try prototype.createMethodProperty(PropertyKey.from("constructor"), Value.from(function));
 
     // 19. If ClassBody[opt] is not present, let elements be a new empty List.
-    // TODO: 20. Else, let elements be NonConstructorElements of ClassBody.
-    const elements = class_tail.class_body.class_element_list.items;
+    // 20. Else, let elements be NonConstructorElements of ClassBody.
+    const elements = try class_tail.class_body.nonConstructorElements(agent.gc_allocator);
+    agent.gc_allocator.free(elements);
 
     // TODO: 21-22.
 

src/language/ast.zig
@@ -4566,6 +4566,33 @@ pub const ClassBody = struct {
         return null;
     }
 
+    /// 15.7.5 Static Semantics: NonConstructorElements
+    /// https://tc39.es/ecma262/#sec-static-semantics-nonconstructorelements
+    pub fn nonConstructorElements(
+        self: Self,
+        allocator: Allocator,
+    ) Allocator.Error![]const ClassElement {
+        // ClassElementList : ClassElement
+        // 1. If ClassElementKind of ClassElement is non-constructor-method, then
+        //     a. Return « ClassElement ».
+        // 2. Return a new empty List.
+        // ClassElementList : ClassElementList ClassElement
+        // 1. Let list be NonConstructorElements of ClassElementList.
+        // 2. If ClassElementKind of ClassElement is non-constructor-method, then
+        //     a. Append ClassElement to the end of list.
+        // 3. Return list.
+        var class_elements = try std.ArrayList(ClassElement).initCapacity(
+            allocator,
+            self.class_element_list.items.len,
+        );
+        for (self.class_element_list.items) |class_element| {
+            if (class_element.classElementKind() == .non_constructor_method) {
+                class_elements.appendAssumeCapacity(class_element);
+            }
+        }
+        return class_elements.toOwnedSlice();
+    }
+
     pub fn print(self: Self, writer: anytype, indentation: usize) @TypeOf(writer).Error!void {
         // Omit printing 'ClassBody' here, it's implied and only adds nesting.
         try self.class_element_list.print(writer, indentation);

commit 4baa7d839a738c987e98b7a8ea900ed9fb492d4d

language: Fix evaluation of non-constructor class elements
A missing `defer` can have catastrophic consequences.

This regressed in c7996832e.

src/language/bytecode/Vm.zig
@@ -1544,7 +1544,7 @@ fn classDefinitionEvaluation(
     // 19. If ClassBody[opt] is not present, let elements be a new empty List.
     // 20. Else, let elements be NonConstructorElements of ClassBody.
     const elements = try class_tail.class_body.nonConstructorElements(agent.gc_allocator);
-    agent.gc_allocator.free(elements);
+    defer agent.gc_allocator.free(elements);
 
     // TODO: 21-22.
 