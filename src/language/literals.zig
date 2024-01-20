braheezy/kiesel-fork

commit 62632c2a3e3a7494c66c78a63f414b9ba7242acc

language: Property lookup after trailing number fraction period

src/language/literals.zig
@@ -114,7 +114,7 @@ pub fn parseNumericLiteral(
         },
         '.' => switch (state) {
             .start => state = .fraction_period,
-            .integer_digit, .fraction_digit, .exponent_digit => {
+            .integer_digit, .fraction_digit, .fraction_period, .exponent_digit => {
                 if (state == .integer_digit and system == .decimal and production == .regular) {
                     state = .fraction_period;
                 } else switch (consume) {

commit d7f728062f1f3a89c5bf43f40927294338827857

language: Fix '..' being parsed as a numeric literal
This regressed in 62632c2a3.

src/language/literals.zig
@@ -117,6 +117,10 @@ pub fn parseNumericLiteral(
             .integer_digit, .fraction_digit, .fraction_period, .exponent_digit => {
                 if (state == .integer_digit and system == .decimal and production == .regular) {
                     state = .fraction_period;
+                } else if (state == .fraction_period and i == 1) {
+                    // Starting fraction period followed by another period, not actually a numeric
+                    // literal (happens in spead expressions)
+                    return error.InvalidNumericLiteral;
                 } else switch (consume) {
                     // Start of a member expression, terminate the literal
                     .partial => return .{