ce2e81fb2618f5619ea36900bf2f48fe0dc5a136
Linus Groh <mail@linusgroh.de>
2024-03-12 21:23:43 +0000

    build: Add a 'docs' step to generate API docs

--- build.zig
+++ build.zig
@@ -147,4 +147,20 @@ pub fn build(b: *std.Build) void {
 
     const test_step = b.step("test", "Run unit tests");
     test_step.dependOn(&run_unit_tests.step);
+
+    const docs = b.addObject(.{
+        .name = "kiesel",
+        .root_source_file = .{ .path = "src/kiesel.zig" },
+        .target = target,
+        .optimize = optimize,
+    });
+    for (imports.items) |import| docs.root_module.addImport(import.name, import.module);
+    const install_docs = b.addInstallDirectory(.{
+        .source_dir = docs.getEmittedDocs(),
+        .install_dir = .prefix,
+        .install_subdir = "docs",
+    });
+
+    const docs_step = b.step("docs", "Build and install documentation");
+    docs_step.dependOn(&install_docs.step);
 }
