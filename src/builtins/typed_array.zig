braheezy/kiesel-fork

commit d85982a0cd124ddcbf55d7f6acdfe4ecf7ca779e

builtins: Bring {%TA%,Array}#join() and Array#toLocaleString() in sync
ECMA-262-Changeset: https://github.com/tc39/ecma262/commit/c579bc227723e50e3a26604702e0fd7fb15db168

src/builtins/typed_array.zig
@@ -1672,14 +1672,18 @@ pub const TypedArrayPrototype = struct {
             // b. Let element be ! Get(O, ! ToString(𝔽(k))).
             const element = object.get(PropertyKey.from(k)) catch |err| try noexcept(err);
 
-            // c. If element is undefined, let next be the empty String; otherwise, let next be
-            //    ! ToString(element).
-            const next = if (element == .undefined) "" else (try element.toString(agent)).utf8;
+            // c. If element is not undefined, then
+            if (element == .undefined) {
+                // i. Let S be ! ToString(element).
+                const string = element.toString(agent) catch |err| try noexcept(err);
 
-            // d. Set R to the string-concatenation of R and next.
-            try elements.append(next);
+                // ii. Set R to the string-concatenation of R and S.
+                try elements.append(string.utf8);
+            } else {
+                try elements.append("");
+            }
 
-            // e. Set k to k + 1.
+            // d. Set k to k + 1.
         }
 
         // 9. Return R.

src/builtins/array.zig
@@ -1699,17 +1699,18 @@ pub const ArrayPrototype = struct {
             // b. Let element be ? Get(O, ! ToString(𝔽(k))).
             const element = try object.get(PropertyKey.from(k));
 
-            // c. If element is either undefined or null, let next be the empty String; otherwise,
-            //    let next be ? ToString(element).
-            const next = if (element == .undefined or element == .null)
-                ""
-            else
-                (try element.toString(agent)).utf8;
+            // c. If element is neither undefined nor null, then
+            if (element != .undefined and element != .null) {
+                // i. Let S be ? ToString(element).
+                const string = try element.toString(agent);
 
-            // d. Set R to the string-concatenation of R and next.
-            try elements.append(next);
+                // ii. Set R to the string-concatenation of R and S.
+                try elements.append(string.utf8);
+            } else {
+                try elements.append("");
+            }
 
-            // e. Set k to k + 1.
+            // d. Set k to k + 1.
         }
 
         // 8. Return R.
@@ -2669,16 +2670,15 @@ pub const ArrayPrototype = struct {
 
         // 6. Repeat, while k < len,
         while (k < len) : (k += 1) {
-            // a. If k > 0, then
-            // i. Set R to the string-concatenation of R and separator.
+            // a. If k > 0, set R to the string-concatenation of R and separator.
 
-            // b. Let nextElement be ? Get(array, ! ToString(𝔽(k))).
-            const next_element = try array.get(PropertyKey.from(k));
+            // b. Let element be ? Get(array, ! ToString(𝔽(k))).
+            const element = try array.get(PropertyKey.from(k));
 
-            // c. If nextElement is neither undefined nor null, then
-            if (next_element != .undefined and next_element != .null) {
-                // i. Let S be ? ToString(? Invoke(nextElement, "toLocaleString")).
-                const string = try (try next_element.invokeNoArgs(
+            // c. If element is neither undefined nor null, then
+            if (element != .undefined and element != .null) {
+                // i. Let S be ? ToString(? Invoke(element, "toLocaleString")).
+                const string = try (try element.invokeNoArgs(
                     agent,
                     PropertyKey.from("toLocaleString"),
                 )).toString(agent);

commit 6017999d1d443e4d68319a7181a87336c8d41169

builtins: Fix logic typo in %TypedArray%.prototype.join()
This regressed in d85982a0c.

src/builtins/typed_array.zig
@@ -1673,7 +1673,7 @@ pub const TypedArrayPrototype = struct {
             const element = object.get(PropertyKey.from(k)) catch |err| try noexcept(err);
 
             // c. If element is not undefined, then
-            if (element == .undefined) {
+            if (element != .undefined) {
                 // i. Let S be ! ToString(element).
                 const string = element.toString(agent) catch |err| try noexcept(err);
 