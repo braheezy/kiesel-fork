braheezy/kiesel-fork

commit 6e626975c93262775444aafb120f26cf87553bd5

builtins: Move {Shared,}ArrayBuffer size limit to data block creation
This ensures the RangeError is only thrown after the object was created
(which is observably fallible by itself).

src/builtins/array_buffer.zig
@@ -68,13 +68,6 @@ pub fn allocateArrayBuffer(
     byte_length: u64,
     max_byte_length: ?u53,
 ) Agent.Error!Object {
-    // Non-standard but present in other engines (and tested by test262)
-    if (byte_length > ArrayBuffer.Fields.max_byte_length or
-        (max_byte_length orelse 0) > ArrayBuffer.Fields.max_byte_length)
-    {
-        return agent.throwException(.range_error, "Maximum buffer size exceeded", .{});
-    }
-
     // 1. Let slots be « [[ArrayBufferData]], [[ArrayBufferByteLength]], [[ArrayBufferDetachKey]] ».
 
     // 2. If maxByteLength is present and maxByteLength is not empty, let allocatingResizableBuffer
@@ -992,9 +985,6 @@ pub const ArrayBufferPrototype = struct {
 /// https://tc39.es/ecma262/#sec-properties-of-the-arraybuffer-instances
 pub const ArrayBuffer = MakeObject(.{
     .Fields = struct {
-        /// Arbitrary size limit (32 GiB)
-        const max_byte_length = 1024 * 1024 * 1024 * 32;
-
         /// [[ArrayBufferData]]
         /// [[ArrayBufferByteLength]]
         array_buffer_data: ?DataBlock,

src/builtins/shared_array_buffer.zig
@@ -38,13 +38,6 @@ pub fn allocateSharedArrayBuffer(
     byte_length: u64,
     max_byte_length: ?u53,
 ) Agent.Error!Object {
-    // Non-standard but present in other engines (and tested by test262)
-    if (byte_length > SharedArrayBuffer.Fields.max_byte_length or
-        (max_byte_length orelse 0) > SharedArrayBuffer.Fields.max_byte_length)
-    {
-        return agent.throwException(.range_error, "Maximum buffer size exceeded", .{});
-    }
-
     // 1. Let slots be « [[ArrayBufferData]] ».
 
     // 2. If maxByteLength is present and maxByteLength is not empty, let allocatingGrowableBuffer
@@ -365,9 +358,6 @@ pub const SharedArrayBufferPrototype = struct {
 /// https://tc39.es/ecma262/#sec-properties-of-the-sharedarraybuffer-instances
 pub const SharedArrayBuffer = MakeObject(.{
     .Fields = struct {
-        /// Arbitrary size limit (32 GiB)
-        const max_byte_length = 1024 * 1024 * 1024 * 32;
-
         /// [[ArrayBufferData]]
         /// [[ArrayBufferByteLength]]
         array_buffer_data: DataBlock,

src/types/spec/data_block.zig
@@ -9,12 +9,19 @@ const Agent = execution.Agent;
 
 pub const DataBlock = std.ArrayList(u8);
 
+/// Arbitrary size limit (32 GiB)
+pub const max_byte_length = 1024 * 1024 * 1024 * 32;
+
 /// 6.2.9.1 CreateByteDataBlock ( size )
 /// https://tc39.es/ecma262/#sec-createbytedatablock
 pub fn createByteDataBlock(agent: *Agent, size: u64) Agent.Error!DataBlock {
     // 1. If size > 2**53 - 1, throw a RangeError exception.
-    if (size > std.math.maxInt(u53)) {
-        return agent.throwException(.range_error, "Maximum buffer size exceeded", .{});
+    comptime std.debug.assert(max_byte_length <= std.math.maxInt(u53));
+
+    // NOTE: Checking for a reasonable size below the theoretical limit is non-standard but also
+    //       done in other engines (and tested by test262)
+    if (size > max_byte_length) {
+        return agent.throwException(.range_error, "Cannot allocate buffer of size {}", .{size});
     }
 
     const size_casted = std.math.cast(usize, size) orelse {
@@ -38,6 +45,12 @@ pub fn createByteDataBlock(agent: *Agent, size: u64) Agent.Error!DataBlock {
 /// 6.2.9.2 CreateSharedByteDataBlock ( size )
 /// https://tc39.es/ecma262/#sec-createsharedbytedatablock
 pub fn createSharedByteDataBlock(agent: *Agent, size: u64) Agent.Error!DataBlock {
+    // NOTE: Checking for a reasonable size below the theoretical limit is non-standard but also
+    //       done in other engines (and tested by test262)
+    if (size > max_byte_length) {
+        return agent.throwException(.range_error, "Cannot allocate buffer of size {}", .{size});
+    }
+
     const size_casted = std.math.cast(usize, size) orelse {
         return agent.throwException(.range_error, "Cannot allocate buffer of size {}", .{size});
     };

commit 97035ae99618c999fe354e5ebb1549e0b8c167a6

builtins: Restore non-standard {Shared,}ArrayBuffer maxByteLength check
This regressed in 6e626975c.

src/builtins/array_buffer.zig
@@ -30,6 +30,7 @@ const defineBuiltinProperty = utils.defineBuiltinProperty;
 const noexcept = utils.noexcept;
 const ordinaryCreateFromConstructor = builtins.ordinaryCreateFromConstructor;
 const sameValue = types.sameValue;
+const data_block_max_byte_length = types.data_block_max_byte_length;
 
 pub const ArrayBufferLike = union(enum) {
     const Self = @This();
@@ -78,6 +79,12 @@ pub fn allocateArrayBuffer(
     if (allocating_resizable_buffer) {
         // a. If byteLength > maxByteLength, throw a RangeError exception.
         if (byte_length > max_byte_length.?) {
+            return agent.throwException(.range_error, "Maximum buffer size exceeded", .{});
+        }
+
+        // NOTE: Checking for a reasonable size below the theoretical limit is non-standard but also
+        //       done in other engines (and tested by test262)
+        if (max_byte_length.? > data_block_max_byte_length) {
             return agent.throwException(.range_error, "Maximum buffer size exceeded", .{});
         }
 

src/builtins/shared_array_buffer.zig
@@ -29,6 +29,7 @@ const defineBuiltinProperty = utils.defineBuiltinProperty;
 const getArrayBufferMaxByteLengthOption = builtins.getArrayBufferMaxByteLengthOption;
 const isFixedLengthArrayBuffer = builtins.isFixedLengthArrayBuffer;
 const ordinaryCreateFromConstructor = builtins.ordinaryCreateFromConstructor;
+const data_block_max_byte_length = types.data_block_max_byte_length;
 
 /// 25.2.2.1 AllocateSharedArrayBuffer ( constructor, byteLength [ , maxByteLength ] )
 /// https://tc39.es/ecma262/#sec-allocatesharedarraybuffer
@@ -48,6 +49,12 @@ pub fn allocateSharedArrayBuffer(
     if (allocating_growable_buffer) {
         // a. If byteLength > maxByteLength, throw a RangeError exception.
         if (byte_length > max_byte_length.?) {
+            return agent.throwException(.range_error, "Maximum buffer size exceeded", .{});
+        }
+
+        // NOTE: Checking for a reasonable size below the theoretical limit is non-standard but also
+        //       done in other engines (and tested by test262)
+        if (max_byte_length.? > data_block_max_byte_length) {
             return agent.throwException(.range_error, "Maximum buffer size exceeded", .{});
         }
 

src/types.zig
@@ -42,6 +42,7 @@ pub const isLooselyEqual = language.isLooselyEqual;
 pub const isStrictlyEqual = language.isStrictlyEqual;
 pub const sameValue = language.sameValue;
 pub const sameValueZero = language.sameValueZero;
+pub const data_block_max_byte_length = spec.data_block_max_byte_length;
 
 pub const SafePointer = @import("any-pointer").SafePointer;
 

src/types/spec.zig
@@ -20,6 +20,7 @@ pub const createIterResultObject = iterator.createIterResultObject;
 pub const createSharedByteDataBlock = data_block.createSharedByteDataBlock;
 pub const getIterator = iterator.getIterator;
 pub const getIteratorFromMethod = iterator.getIteratorFromMethod;
+pub const data_block_max_byte_length = data_block.data_block_max_byte_length;
 
 test {
     _ = data_block;

src/types/spec/data_block.zig
@@ -10,17 +10,17 @@ const Agent = execution.Agent;
 pub const DataBlock = std.ArrayList(u8);
 
 /// Arbitrary size limit (32 GiB)
-pub const max_byte_length = 1024 * 1024 * 1024 * 32;
+pub const data_block_max_byte_length = 1024 * 1024 * 1024 * 32;
 
 /// 6.2.9.1 CreateByteDataBlock ( size )
 /// https://tc39.es/ecma262/#sec-createbytedatablock
 pub fn createByteDataBlock(agent: *Agent, size: u64) Agent.Error!DataBlock {
     // 1. If size > 2**53 - 1, throw a RangeError exception.
-    comptime std.debug.assert(max_byte_length <= std.math.maxInt(u53));
+    comptime std.debug.assert(data_block_max_byte_length <= std.math.maxInt(u53));
 
     // NOTE: Checking for a reasonable size below the theoretical limit is non-standard but also
     //       done in other engines (and tested by test262)
-    if (size > max_byte_length) {
+    if (size > data_block_max_byte_length) {
         return agent.throwException(.range_error, "Cannot allocate buffer of size {}", .{size});
     }
 
@@ -47,7 +47,7 @@ pub fn createByteDataBlock(agent: *Agent, size: u64) Agent.Error!DataBlock {
 pub fn createSharedByteDataBlock(agent: *Agent, size: u64) Agent.Error!DataBlock {
     // NOTE: Checking for a reasonable size below the theoretical limit is non-standard but also
     //       done in other engines (and tested by test262)
-    if (size > max_byte_length) {
+    if (size > data_block_max_byte_length) {
         return agent.throwException(.range_error, "Cannot allocate buffer of size {}", .{size});
     }
 