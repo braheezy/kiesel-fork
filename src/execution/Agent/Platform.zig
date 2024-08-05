9524359afde26381994aeb33018a62b621e9d94b
Linus Groh <mail@linusgroh.de>
2024-08-05 20:39:14 +0100

    execution: Fix Agent.Platform.default() on windows
    Also add a comptime check to ensure this doesn't regress again.

--- src/execution/Agent/Platform.zig
+++ src/execution/Agent/Platform.zig
@@ -1,3 +1,4 @@
+const builtin = @import("builtin");
 const std = @import("std");
 
 const icu4zig = @import("icu4zig");
@@ -14,15 +15,35 @@ stack_info: ?stackinfo.StackInfo,
 default_locale: if (build_options.enable_intl) icu4zig.Locale else void,
 currentTime: *const fn () i64,
 
+/// Whether `fd_t` is defined
 const has_fd_t = @hasDecl(std.posix.system, "fd_t");
+
+/// Whether `clockid_t` is defined
 const has_clockid_t = @hasDecl(std.posix.system, "clockid_t") and
     std.posix.system.clockid_t != void;
+
+/// Whether `std.time.nanoTimestamp()` is usable, which is what `std.time.milliTimestamp()` uses
+const has_std_time_nanotimestamp = switch (builtin.os.tag) {
+    // These have custom implementations
+    .windows, .wasi, .uefi => true,
+    // The fallback is `posix.clock_gettime()`, which is always defined when linking libc but
+    // relies on `clockid_t` existing.
+    else => has_clockid_t,
+};
 
 // `any()` captures a pointer to the writer, so these have to stick around.
 var stdout_writer: if (has_fd_t) std.fs.File.Writer else void = undefined;
 var stderr_writer: if (has_fd_t) std.fs.File.Writer else void = undefined;
 
-pub const default = if (has_clockid_t and has_fd_t) defaultImpl else {};
+pub const default = if (has_fd_t and has_std_time_nanotimestamp) defaultImpl else {};
+
+// Ensure the default implementation doesn't silently break on known supported platforms
+comptime {
+    switch (builtin.os.tag) {
+        .linux, .macos, .windows, .wasi => std.debug.assert(@TypeOf(default) != void),
+        else => {},
+    }
+}
 
 fn defaultImpl() Self {
     stdout_writer = std.io.getStdOut().writer();
