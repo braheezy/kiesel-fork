braheezy/kiesel-fork

commit 1538bdb790d3c77f938da29882f023ea9cdade35

language: Support binding pattern in assignment expression

src/language/bytecode/codegen.zig
@@ -1594,30 +1594,47 @@ pub fn codegenAssignmentExpression(
     // AssignmentExpression : LeftHandSideExpression = AssignmentExpression
     if (node.operator == .@"=") {
         // 1. If LeftHandSideExpression is neither an ObjectLiteral nor an ArrayLiteral, then
+        if (node.lhs_expression.* != .binding_pattern_for_assignment_expression) {
+            // a. Let lRef be ? Evaluation of LeftHandSideExpression.
+            try codegenExpression(node.lhs_expression.*, executable, ctx);
+            try executable.addInstruction(.push_reference);
 
-        // a. Let lRef be ? Evaluation of LeftHandSideExpression.
-        try codegenExpression(node.lhs_expression.*, executable, ctx);
-        try executable.addInstruction(.push_reference);
+            // TODO: b. If IsAnonymousFunctionDefinition(AssignmentExpression) and IsIdentifierRef of
+            //          LeftHandSideExpression are both true, then
+            if (false) {
+                // i. Let lhs be the StringValue of LeftHandSideExpression.
+                // ii. Let rVal be ? NamedEvaluation of AssignmentExpression with argument lhs.
+            }
+            // c. Else,
+            else {
+                // i. Let rRef be ? Evaluation of AssignmentExpression.
+                try codegenExpression(node.rhs_expression.*, executable, ctx);
 
-        // TODO: b. If IsAnonymousFunctionDefinition(AssignmentExpression) and IsIdentifierRef of
-        //          LeftHandSideExpression are both true, then
-        if (false) {
-            // i. Let lhs be the StringValue of LeftHandSideExpression.
-            // ii. Let rVal be ? NamedEvaluation of AssignmentExpression with argument lhs.
+                // ii. Let rVal be ? GetValue(rRef).
+                if (node.rhs_expression.analyze(.is_reference)) try executable.addInstruction(.get_value);
+            }
+
+            // d. Perform ? PutValue(lRef, rVal).
+            // e. Return rVal.
+            try executable.addInstruction(.put_value);
+            try executable.addInstruction(.pop_reference);
         }
-        // c. Else,
-        else {
-            // i. Let rRef be ? Evaluation of AssignmentExpression.
-            try codegenExpression(node.rhs_expression.*, executable, ctx);
 
-            // ii. Let rVal be ? GetValue(rRef).
-            if (node.rhs_expression.analyze(.is_reference)) try executable.addInstruction(.get_value);
-        }
+        // 2. Let assignmentPattern be the AssignmentPattern that is covered by LeftHandSideExpression.
+        const assignment_pattern = node.lhs_expression.binding_pattern_for_assignment_expression;
 
-        // d. Perform ? PutValue(lRef, rVal).
-        // e. Return rVal.
-        try executable.addInstruction(.put_value);
-        try executable.addInstruction(.pop_reference);
+        // 3. Let rRef be ? Evaluation of AssignmentExpression.
+        try codegenExpression(node.rhs_expression.*, executable, ctx);
+
+        // 4. Let rVal be ? GetValue(rRef).
+        if (node.rhs_expression.analyze(.is_reference)) try executable.addInstruction(.get_value);
+
+        try executable.addInstruction(.load);
+
+        // 5. Perform ? DestructuringAssignmentEvaluation of assignmentPattern with argument rVal.
+        // 6. Return rVal.
+        try bindingInitialization(assignment_pattern, executable, ctx, null);
+        try executable.addInstruction(.store);
     }
     // AssignmentExpression : LeftHandSideExpression AssignmentOperator AssignmentExpression
     else if (node.operator != .@"&&=" and node.operator != .@"||=" and node.operator != .@"??=") {
@@ -1871,6 +1888,7 @@ pub fn codegenExpression(
         .await_expression => |x| try codegenAwaitExpression(x, executable, ctx),
         .yield_expression => |x| try codegenYieldExpression(x, executable, ctx),
         .tagged_template => |x| try codegenTaggedTemplate(x, executable, ctx),
+        .binding_pattern_for_assignment_expression => |x| try bindingInitialization(x, executable, ctx, null),
     }
 }
 
@@ -2755,14 +2773,18 @@ fn forInOfBodyEvaluation(
 
     // 4. Let destructuring be IsDestructuring of lhs.
     const destructuring = switch (lhs) {
-        .expression => false, // TODO: Support AssignmentPattern
+        .expression => |expression| expression == .binding_pattern_for_assignment_expression,
         .for_binding => false,
         .for_declaration => |for_declaration| for_declaration.for_binding == .binding_pattern,
     };
 
+    var assignment_pattern: ast.BindingPattern = undefined;
+
     // 5. If destructuring is true and lhsKind is assignment, then
     if (destructuring and lhs_kind == .assignment) {
-        // TODO: a-b.
+        // a. Assert: lhs is a LeftHandSideExpression.
+        // b. Let assignmentPattern be the AssignmentPattern that is covered by lhs.
+        assignment_pattern = lhs.expression.binding_pattern_for_assignment_expression;
     }
 
     // 6. Repeat,
@@ -2813,8 +2835,10 @@ fn forInOfBodyEvaluation(
         if (destructuring) {
             // 1. If lhsKind is assignment, then
             if (lhs_kind == .assignment) {
-                // TODO: a. Let status be Completion(DestructuringAssignmentEvaluation of
-                //          assignmentPattern with argument nextValue).
+                // a. Let status be Completion(DestructuringAssignmentEvaluation of
+                //    assignmentPattern with argument nextValue).
+                try executable.addInstruction(.load);
+                try bindingInitialization(assignment_pattern, executable, ctx, null);
             }
             // 2. Else,
             else {

src/language/Parser.zig
@@ -1337,7 +1337,9 @@ pub fn acceptAssignmentExpression(
 
     // If LeftHandSideExpression is neither an ObjectLiteral nor an ArrayLiteral, it is a Syntax
     // Error if the AssignmentTargetType of LeftHandSideExpression is not simple.
-    if (primary_expression.assignmentTargetType() != .simple) {
+    if (primary_expression != .binding_pattern_for_assignment_expression and
+        primary_expression.assignmentTargetType() != .simple)
+    {
         try self.emitErrorAt(
             primary_expression_location,
             "Invalid left-hand side in assignment expression",
@@ -1438,6 +1440,40 @@ pub fn acceptExpression(self: *Parser, ctx: AcceptContext) AcceptError!ast.Expre
                     ctx,
                 ),
             };
+        },
+        .@"{", .@"[" => blk: {
+            const tmp_state = self.core.saveState();
+            if (self.acceptBindingPattern()) |binding_pattern| {
+                if (try self.core.peek()) |next_token_| switch (next_token_.type) {
+                    // Assignment expression LHS
+                    .@"=",
+                    .@"*=",
+                    .@"/=",
+                    .@"%=",
+                    .@"+=",
+                    .@"-=",
+                    .@"<<=",
+                    .@">>=",
+                    .@">>>=",
+                    .@"&=",
+                    .@"^=",
+                    .@"|=",
+                    .@"**=",
+                    .@"&&=",
+                    .@"||=",
+                    .@"??=",
+                    // For loop initializer
+                    .in,
+                    .identifier,
+                    => if (next_token_.type != .identifier or std.mem.eql(u8, next_token_.text, "of")) {
+                        break :blk .{ .binding_pattern_for_assignment_expression = binding_pattern };
+                    },
+                    else => {},
+                };
+            } else |_| {}
+            self.core.restoreState(tmp_state);
+            const primary_expression = try self.acceptPrimaryExpression();
+            break :blk .{ .primary_expression = primary_expression };
         },
         else => if (self.acceptAwaitExpression()) |await_expression|
             .{ .await_expression = await_expression }
@@ -2014,7 +2050,10 @@ pub fn acceptForInOfStatement(self: *Parser) AcceptError!ast.ForInOfStatement {
 
     // If LeftHandSideExpression is neither an ObjectLiteral nor an ArrayLiteral, it is a Syntax
     // Error if the AssignmentTargetType of LeftHandSideExpression is not simple.
-    if (initializer == .expression and initializer.expression.assignmentTargetType() != .simple) {
+    if (initializer == .expression and
+        initializer.expression != .binding_pattern_for_assignment_expression and
+        initializer.expression.assignmentTargetType() != .simple)
+    {
         try self.emitErrorAt(
             initializer_location,
             "Invalid 'for in'/'for of' loop initializer expression",

src/language/ast.zig
@@ -727,6 +727,8 @@ pub const Expression = union(enum) {
     await_expression: AwaitExpression,
     yield_expression: YieldExpression,
     tagged_template: TaggedTemplate,
+    /// Modern problems (binding pattern LHS) require modern solutions (fake expression type)
+    binding_pattern_for_assignment_expression: BindingPattern,
 
     /// 8.6.4 Static Semantics: AssignmentTargetType
     /// https://tc39.es/ecma262/#sec-static-semantics-assignmenttargettype

src/language/ast_printing.zig
@@ -324,6 +324,7 @@ pub fn printExpression(node: ast.Expression, writer: anytype, indentation: usize
         .await_expression => |x| try printAwaitExpression(x, writer, indentation + 1),
         .yield_expression => |x| try printYieldExpression(x, writer, indentation + 1),
         .tagged_template => |x| try printTaggedTemplate(x, writer, indentation + 1),
+        .binding_pattern_for_assignment_expression => |x| try printBindingPattern(x, writer, indentation + 1),
     }
 }
 

commit 5758dcd04d7e850796b81d69ab4c21114ca5caff

language: Add missing early return in assignment expression codegen
Regressed in 1538bdb79.

src/language/bytecode/codegen.zig
@@ -1618,6 +1618,7 @@ pub fn codegenAssignmentExpression(
             // e. Return rVal.
             try executable.addInstruction(.put_value);
             try executable.addInstruction(.pop_reference);
+            return;
         }
 
         // 2. Let assignmentPattern be the AssignmentPattern that is covered by LeftHandSideExpression.