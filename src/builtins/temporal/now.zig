05b062d3c3ddff05ad2fc5695dea91f49be1ec68
Linus Groh <mail@linusgroh.de>
2025-06-16 00:28:39 +0200

    builtins: Implement Temporal.Now.timeZoneId()

--- src/builtins/temporal/now.zig
+++ src/builtins/temporal/now.zig
@@ -8,8 +8,10 @@ const execution = @import("../../execution.zig");
 const types = @import("../../types.zig");
 
 const Agent = execution.Agent;
+const Arguments = types.Arguments;
 const Object = types.Object;
 const Realm = execution.Realm;
+const String = types.String;
 const Value = types.Value;
 
 pub const namespace = struct {
@@ -19,7 +21,9 @@ pub const namespace = struct {
         });
     }
 
-    pub fn init(agent: *Agent, _: *Realm, object: *Object) std.mem.Allocator.Error!void {
+    pub fn init(agent: *Agent, realm: *Realm, object: *Object) std.mem.Allocator.Error!void {
+        try object.defineBuiltinFunction(agent, "timeZoneId", timeZoneId, 0, realm);
+
         // 2.1.1 Temporal.Now [ %Symbol.toStringTag% ]
         // https://tc39.es/proposal-temporal/#sec-temporal-now-%symbol.tostringtag%
         try object.defineBuiltinPropertyWithAttributes(
@@ -32,5 +36,12 @@ pub const namespace = struct {
                 .configurable = true,
             },
         );
+    }
+
+    /// 2.2.1 Temporal.Now.timeZoneId ( )
+    /// https://tc39.es/proposal-temporal/#sec-temporal.now.timezoneid
+    fn timeZoneId(agent: *Agent, _: Value, _: Arguments) Agent.Error!Value {
+        // 1. Return SystemTimeZoneIdentifier().
+        return Value.from(try String.fromAscii(agent, builtins.systemTimeZoneIdentifier()));
     }
 };
