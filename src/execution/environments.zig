braheezy/kiesel-fork

commit 7157d1a266324ab1a24d33dee8d2225a65101043

execution: Require lookup cache in getIdentifierReference()

src/execution/environments.zig
@@ -191,7 +191,7 @@ pub fn getIdentifierReference(
     start_env: Environment,
     name: *const String,
     strict: bool,
-    maybe_lookup_cache_entry: ?*?Environment.LookupCacheEntry,
+    lookup_cache_entry: *?Environment.LookupCacheEntry,
 ) Agent.Error!Reference {
     // 1. If env is null, then
     //     a. Return the Reference Record {
@@ -206,11 +206,23 @@ pub fn getIdentifierReference(
     //     a. Let outer be env.[[OuterEnv]].
     //     b. Return ? GetIdentifierReference(outer, name, strict).
     var env = start_env;
-    if (maybe_lookup_cache_entry) |lookup_cache_entry| {
-        if (lookup_cache_entry.*) |cache| {
-            // In the case of an unresolvable reference we'll reach the last environment without an
-            // outer env.
-            for (0..cache.distance) |_| env = env.outerEnv() orelse {
+    if (lookup_cache_entry.*) |cache| {
+        // In the case of an unresolvable reference we'll reach the last environment without an
+        // outer env.
+        for (0..cache.distance) |_| env = env.outerEnv() orelse {
+            @branchHint(.unlikely);
+            return .{
+                .base = .unresolvable,
+                .referenced_name = .{ .value = Value.from(name) },
+                .strict = strict,
+                .this_value = null,
+            };
+        };
+    } else {
+        var distance: usize = 0;
+        defer lookup_cache_entry.* = .{ .distance = distance };
+        while (!try env.hasBinding(agent, name)) : (distance += 1) {
+            env = env.outerEnv() orelse {
                 @branchHint(.unlikely);
                 return .{
                     .base = .unresolvable,
@@ -220,22 +232,6 @@ pub fn getIdentifierReference(
                 };
             };
         }
-    }
-    var distance: usize = 0;
-    defer if (maybe_lookup_cache_entry) |lookup_cache_entry| {
-        lookup_cache_entry.* = .{ .distance = distance };
-    };
-    while (!try env.hasBinding(agent, name)) {
-        distance += 1;
-        env = env.outerEnv() orelse {
-            @branchHint(.unlikely);
-            return .{
-                .base = .unresolvable,
-                .referenced_name = .{ .value = Value.from(name) },
-                .strict = strict,
-                .this_value = null,
-            };
-        };
     }
     return .{
         .base = .{ .environment = env },

src/language/bytecode/Vm.zig
@@ -1766,14 +1766,13 @@ fn executeResolveBindingDirect(
         }
     } else {
         var distance: usize = 0;
-        while (!try env.hasBinding(self.agent, name)) {
+        defer lookup_cache_entry.* = .{ .distance = distance };
+        while (!try env.hasBinding(self.agent, name)) : (distance += 1) {
             env = env.outerEnv() orelse {
                 @branchHint(.unlikely);
                 return self.agent.throwException(.reference_error, "'{f}' is not defined", .{name.fmtRaw()});
             };
-            distance += 1;
         }
-        lookup_cache_entry.* = .{ .distance = distance };
     }
     self.result = try env.getBindingValue(self.agent, name, strict);
 }

src/execution/Agent.zig
@@ -270,7 +270,7 @@ pub fn resolveBinding(
     name: *const String,
     maybe_env: ?Environment,
     strict: bool,
-    maybe_lookup_cache_entry: ?*?Environment.LookupCacheEntry,
+    lookup_cache_entry: *?Environment.LookupCacheEntry,
 ) Error!Reference {
     // 1. If env is not present or env is undefined, then
     //     a. Set env to the running execution context's LexicalEnvironment.
@@ -281,7 +281,7 @@ pub fn resolveBinding(
     // NOTE: This is part of the generated bytecode and passed through as an argument.
 
     // 4. Return ? GetIdentifierReference(env, name, strict).
-    return getIdentifierReference(self, env, name, strict, maybe_lookup_cache_entry);
+    return getIdentifierReference(self, env, name, strict, lookup_cache_entry);
 }
 
 /// 9.4.3 GetThisEnvironment ( )

src/language/runtime.zig
@@ -69,7 +69,8 @@ pub fn initializeBoundName(
         // 2. Else,
         .strict => |strict| {
             // a. Let lhs be ? ResolveBinding(name).
-            const lhs = try agent.resolveBinding(name, null, strict, null);
+            var dummy_cache: ?Environment.LookupCacheEntry = null;
+            const lhs = try agent.resolveBinding(name, null, strict, &dummy_cache);
 
             // b. Return ? PutValue(lhs, value).
             try lhs.putValue(agent, value);

commit 47f0095615c469e2189da9eb3e75dab981098aa4

execution: Restore binding check when using environment lookup cache
Regressed in 7157d1a26. This broke a 'typeof window' check in v8-v7 when
running the second test due to the cached distance pointing to the
global environment instead of returning an unresolvable reference like
during the initial run.

src/execution/environments.zig
@@ -208,10 +208,17 @@ pub fn getIdentifierReference(
     //     a. Let outer be env.[[OuterEnv]].
     //     b. Return ? GetIdentifierReference(outer, name, strict).
     var env = start_env;
+    var distance: u16 = 0;
+    defer lookup_cache_entry.* = .{ .distance = distance };
     if (lookup_cache_entry.*) |cache| {
-        // In the case of an unresolvable reference we'll reach the last environment without an
-        // outer env.
-        for (0..cache.distance) |_| env = env.outerEnv() orelse {
+        while (distance < cache.distance) : (distance += 1) {
+            // We only ever increase the distance up to the global env.
+            // If we do reach it, the while loop below will check if the binding still exists.
+            env = env.outerEnv().?;
+        }
+    }
+    while (!try env.hasBinding(agent, name)) : (distance += 1) {
+        env = env.outerEnv() orelse {
             @branchHint(.unlikely);
             return .{
                 .base = .unresolvable,
@@ -220,20 +227,6 @@ pub fn getIdentifierReference(
                 .this_value = null,
             };
         };
-    } else {
-        var distance: u16 = 0;
-        defer lookup_cache_entry.* = .{ .distance = distance };
-        while (!try env.hasBinding(agent, name)) : (distance += 1) {
-            env = env.outerEnv() orelse {
-                @branchHint(.unlikely);
-                return .{
-                    .base = .unresolvable,
-                    .referenced_name = .{ .value = Value.from(name) },
-                    .strict = strict,
-                    .this_value = null,
-                };
-            };
-        }
     }
     return .{
         .base = .{ .environment = env },

src/language/bytecode/Vm.zig
@@ -1757,22 +1757,20 @@ fn executeResolveBindingDirect(
     const name = self.executable.getIdentifier(name_index);
     const lookup_cache_entry = self.executable.getEnvironmentLookupCacheEntry(environment_lookup_cache_index);
     var env = self.agent.runningExecutionContext().ecmascript_code.lexical_environment;
+    var distance: u16 = 0;
+    defer lookup_cache_entry.* = .{ .distance = distance };
     if (lookup_cache_entry.*) |cache| {
-        for (0..cache.distance) |_| {
-            env = env.outerEnv() orelse {
-                @branchHint(.unlikely);
-                return self.agent.throwException(.reference_error, "'{f}' is not defined", .{name.fmtRaw()});
-            };
+        while (distance < cache.distance) : (distance += 1) {
+            // We only ever increase the distance up to the global env.
+            // If we do reach it, the while loop below will check if the binding still exists.
+            env = env.outerEnv().?;
         }
-    } else {
-        var distance: u16 = 0;
-        defer lookup_cache_entry.* = .{ .distance = distance };
-        while (!try env.hasBinding(self.agent, name)) : (distance += 1) {
-            env = env.outerEnv() orelse {
-                @branchHint(.unlikely);
-                return self.agent.throwException(.reference_error, "'{f}' is not defined", .{name.fmtRaw()});
-            };
-        }
+    }
+    while (!try env.hasBinding(self.agent, name)) : (distance += 1) {
+        env = env.outerEnv() orelse {
+            @branchHint(.unlikely);
+            return self.agent.throwException(.reference_error, "'{f}' is not defined", .{name.fmtRaw()});
+        };
     }
     self.result = try env.getBindingValue(self.agent, name, strict);
 }