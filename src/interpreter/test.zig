27c7bddbf2283bc0301836d37ce1f48d3ab1ba4b
Linus Groh <mail@linusgroh.de>
2026-02-19 20:17:20 +0000

    interpreter: Move test cases into a ZON file

---
+++ src/interpreter/test.zig
@@ -0,0 +1,162 @@
+const std = @import("std");
+
+const builtins = @import("../builtins.zig");
+const execution = @import("../execution.zig");
+const interpreter = @import("../interpreter.zig");
+const language = @import("../language.zig");
+const types = @import("../types.zig");
+
+const Agent = execution.Agent;
+const Bytecode = interpreter.Bytecode;
+const Ir = interpreter.Ir;
+const Parser = language.Parser;
+const Realm = execution.Realm;
+const Script = language.Script;
+const Value = types.Value;
+const Vm = interpreter.Vm;
+
+const ExpectedResult = union(enum) {
+    value: ?LiteralValue,
+    promise: struct { fulfilled: LiteralValue },
+    exception,
+    ignore,
+
+    const LiteralValue = union(enum) {
+        number: i32,
+        string: []const u8,
+
+        fn toValue(comptime self: LiteralValue) Value {
+            return switch (self) {
+                .number => |number| Value.from(number),
+                .string => |string| Value.from(string),
+            };
+        }
+    };
+};
+
+const TestCase = struct {
+    source: []const u8,
+    expected_result: ExpectedResult,
+    expected_ir: ?[]const u8,
+    expected_bc: ?[]const u8,
+};
+
+const test_cases: []const TestCase = @import("test_cases.zon");
+
+fn testInterpreter(
+    gpa: std.mem.Allocator,
+    source: []const u8,
+    comptime expected_result: ExpectedResult,
+    expected_ir: ?[]const u8,
+    expected_bc: ?[]const u8,
+) !void {
+    var arena_instance: std.heap.ArenaAllocator = .init(gpa);
+    defer arena_instance.deinit();
+    const arena = arena_instance.allocator();
+
+    const script = try Parser.parse(language.ast.Script, arena, source, .{});
+
+    var ir = ir: {
+        var builder: Ir.Builder = .init(gpa, "test", .{ .script = &script });
+        defer builder.deinit();
+        break :ir try builder.build();
+    };
+    defer ir.deinit(gpa);
+
+    var bc = bc: {
+        var builder: Bytecode.Builder = try .init(gpa, &ir);
+        defer builder.deinit();
+        break :bc try builder.build();
+    };
+    defer bc.deinit(gpa);
+
+    const platform: Agent.Platform = .default();
+    defer platform.deinit();
+    var agent: Agent = try .init(&platform, .{
+        // Ensure generator code paths use new interpreter
+        .new_interpreter = true,
+    });
+    defer agent.deinit();
+
+    try Realm.initializeHostDefinedRealm(&agent, .{});
+
+    const realm = agent.currentRealm();
+
+    const script_record = try agent.gc_allocator.create(Script);
+    script_record.* = .{
+        .realm = realm,
+        .ecmascript_code = script,
+        .loaded_modules = .empty,
+        .host_defined = .null_pointer,
+    };
+
+    const test_context = try agent.gc_allocator.create(execution.ExecutionContext);
+    test_context.* = .{
+        .origin = .script,
+        .realm = realm,
+        .script_or_module = .{ .script = script_record },
+        .ecmascript_code = .{
+            .variable_environment = .{ .global_environment = realm.global_env },
+            .lexical_environment = .{ .global_environment = realm.global_env },
+            .private_environment = null,
+        },
+    };
+    try agent.execution_context_stack.append(agent.gc_allocator, test_context);
+    defer _ = agent.execution_context_stack.pop().?;
+
+    try Script.globalDeclarationInstantiation(&agent, script, realm.global_env);
+
+    var vm: Vm = try .init(&agent, &bc);
+    defer vm.deinit();
+
+    // Agent.Error!RunResult -> Agent.Error!?Value
+    const unwrapped = if (vm.run(.{})) |result| switch (result) {
+        .@"return" => |value| value,
+        .yield => unreachable,
+    } else |err| err;
+
+    if (unwrapped) |result| switch (expected_result) {
+        .value => |expected| if (expected != null) {
+            if (result == null) return error.TestExpectedEqual;
+            if (!expected.?.toValue().isStrictlyEqual(result.?)) return error.TestExpectedEqual;
+        } else if (result != null) return error.TestExpectedEqual,
+        .promise => |expected| {
+            agent.drainJobQueue();
+            const promise = (result orelse return error.TestExpectedEqual).asObject().as(builtins.Promise);
+            try std.testing.expectEqual(.fulfilled, promise.fields.promise_state);
+            if (!expected.fulfilled.toValue().isStrictlyEqual(promise.fields.promise_result)) return error.TestExpectedEqual;
+        },
+        .exception => return error.TestExpectedException,
+        .ignore => {},
+    } else |err| switch (expected_result) {
+        .exception => try std.testing.expectEqual(error.ExceptionThrown, err),
+        else => return err,
+    }
+
+    var aw: std.Io.Writer.Allocating = .init(gpa);
+    defer aw.deinit();
+
+    if (expected_ir) |expected| {
+        try ir.print(&aw.writer, .no_color);
+        try std.testing.expectEqualStrings(expected, aw.written());
+        aw.clearRetainingCapacity();
+    }
+
+    if (expected_bc) |expected| {
+        try bc.print(&aw.writer, .no_color);
+        try std.testing.expectEqualStrings(expected, aw.written());
+        aw.clearRetainingCapacity();
+    }
+}
+
+test {
+    inline for (test_cases) |test_case| {
+        try testInterpreter(
+            std.testing.allocator,
+            test_case.source,
+            test_case.expected_result,
+            test_case.expected_ir,
+            test_case.expected_bc,
+        );
+    }
+}
