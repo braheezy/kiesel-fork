7bb558c3fbb1da7073a51d1090e720cc09cc13f9
Linus Groh <mail@linusgroh.de>
2026-04-30 00:09:39 +0100

    execution: Remove old feature checks from Agent.Platform
    It seems like none of this is necessary anymore, likely as a result of
    1190cad78 and/or the update to Zig 0.16.

--- src/execution/Agent/Platform.zig
+++ src/execution/Agent/Platform.zig
@@ -1,4 +1,3 @@
-const builtin = @import("builtin");
 const std = @import("std");
 
 const icu4zig = @import("icu4zig");
@@ -22,35 +21,16 @@ default_time_zone: TimeZone,
 pub const Locale = if (build_options.enable_intl) icu4zig.Locale else void;
 pub const TimeZone = if (build_options.enable_temporal) temporal_rs.c.TimeZone else void;
 
-/// Whether `fd_t` is defined
-const has_fd_t = std.posix.system.fd_t != void;
-
-/// Whether `clockid_t` is defined
-const has_clockid_t = @hasDecl(std.posix.system, "clockid_t") and
-    std.posix.system.clockid_t != void;
-
-/// Whether `std.time.nanoTimestamp()` is usable, which is what `std.time.milliTimestamp()` uses
-const has_std_time_nanotimestamp = switch (builtin.os.tag) {
-    // These have custom implementations
-    .windows, .wasi, .uefi => true,
-    // The fallback is `posix.clock_gettime()`, which is always defined when linking libc but
-    // relies on `clockid_t` existing.
-    else => has_clockid_t,
-};
-
 const State = struct {
-    stdout_buffer: if (has_fd_t) [1024]u8 else void,
-    stdout_writer: if (has_fd_t) std.Io.File.Writer else void,
-    stderr_buffer: if (has_fd_t) [1024]u8 else void,
-    stderr_writer: if (has_fd_t) std.Io.File.Writer else void,
+    stdout_buffer: [1024]u8,
+    stdout_writer: std.Io.File.Writer,
+    stderr_buffer: [1024]u8,
+    stderr_writer: std.Io.File.Writer,
 };
 
 var state: State = undefined;
 
 pub fn default(io: std.Io, environ_map: *const std.process.Environ.Map) Platform {
-    if (comptime !(has_fd_t and has_std_time_nanotimestamp)) {
-        @compileError("Platform.default() is not supported on this platform");
-    }
     if (build_options.enable_libgc) gc.init();
     state = .{
         .stdout_buffer = undefined,
